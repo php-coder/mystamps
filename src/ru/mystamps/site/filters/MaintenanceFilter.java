98f6701964b188611ce730bdb09a73856f589785
Slava Semushin <php-coder@altlinux.ru>
2010-01-10 16:36:06 +0600

    MaintenanceFilter: added.
    Addressed to #25

---
+++ src/ru/mystamps/site/filters/MaintenanceFilter.java
@@ -0,0 +1,62 @@
+package ru.mystamps.site.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletException;
+
+import org.apache.log4j.Logger;
+
+public class MaintenanceFilter implements Filter {
+	
+	private FilterConfig config = null;
+	private Logger log = null;
+	
+	public MaintenanceFilter() {
+	}
+	
+	public void init(FilterConfig config) {
+		this.config = config;
+		this.log = Logger.getRootLogger();
+	}
+	
+	public void doFilter(ServletRequest request,
+						ServletResponse response,
+						FilterChain chain)
+				throws IOException, ServletException {
+		
+		String mode = config.getInitParameter("enableMaintainanceMode");
+		if (mode == null || mode.equals("")) {
+			throw new ServletException("enableMaintainanceMode parameter not set!");
+		}
+		
+		String allowedIP = config.getInitParameter("allowedIP");
+		if (allowedIP == null || allowedIP.equals("")) {
+			throw new ServletException("allowedIP parameter not set!");
+		}
+		
+		if (mode.equalsIgnoreCase("yes")) {
+			
+			String userIP = request.getRemoteHost();
+			if (userIP == null || userIP.equals("")) {
+				log.warn("Cannot get user IP address");
+			
+			} else if (! allowedIP.equals(userIP)) {
+				RequestDispatcher dispatch = request.getRequestDispatcher("/maintenance.jsp");
+				dispatch.forward(request, response);
+				return;
+			}
+		}
+		
+		chain.doFilter(request, response);
+	}
+	
+	public void destroy() {
+	}
+}
+
