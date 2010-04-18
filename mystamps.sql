555c9ab8c3b9ceb9e8049f9379d5a888fb638622
Slava Semushin <php-coder@altlinux.ru>
2010-04-18 21:34:49 +0700

    register_user: check e-mail length.
    Addressed to #43

--- mystamps.sql
+++ mystamps.sql
@@ -8,7 +8,7 @@ CREATE TABLE `users` (
 	`id` INT(5) UNSIGNED AUTO_INCREMENT NOT NULL,
 	`login` VARCHAR(15) NOT NULL,
 	`name` VARCHAR(100) NOT NULL,
-	`email` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL, -- see email field at users_activation
 	`registered_at` DATETIME NOT NULL,
 	`activated_at` DATETIME NOT NULL,
 	`status` TINYINT(1) NOT NULL DEFAULT 1, -- -1 (banned), 0 (lost password), 1 (ok)
@@ -22,7 +22,7 @@ CREATE TABLE `users` (
 -- users_activation
 ----
 CREATE TABLE `users_activation` (
-	`email` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL, -- see EMAIL_MAX_LENGTH in WEB-INF/web/.xml
 	`registered_at` DATETIME NOT NULL,
 	`act_key` CHAR(10) NOT NULL,
 	UNIQUE KEY(`act_key`)
