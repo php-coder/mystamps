8b6bbedf7db86ac535a76da7a0ebc164c066b8bd
Slava Semushin <php-coder@altlinux.ru>
2011-01-08 19:33:42 +0600

    User: use @RequiredArgsConstructor annotation from Lombok for generating constructor.
    No functional changes.

--- src/main/java/ru/mystamps/web/entity/User.java
+++ src/main/java/ru/mystamps/web/entity/User.java
@@ -1,16 +1,11 @@
 package ru.mystamps.web.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class User {
 	@Getter private final Long uid;
 	@Getter private final String name;
 	@Getter private final String login;
-	
-	public User(final Long uid, final String name, final String login) {
-		this.uid = uid;		// TODO: should be > 0
-		this.name = name;	// TODO: should be not null and not empty
-		this.login = login;	// TODO: should be not null and not empty
-	}
-	
 }
