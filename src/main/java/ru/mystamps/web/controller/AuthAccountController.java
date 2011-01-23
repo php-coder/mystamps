php-coder/mystamps

commit cbbf655022772e9e575b8b9d250a82e7bcc11cc8

Simplify controllers: use @RequestHeader instead of getHeader().
No functional changes.

src/main/java/ru/mystamps/web/controller/AuthAccountController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.WebDataBinder;
@@ -50,6 +51,8 @@ public class AuthAccountController {
 	public String processInput(
 			final HttpServletRequest request,
 			final HttpSession session,
+			@RequestHeader("referer") final String referer,
+			@RequestHeader("user-agent") final String agent,
 			@Valid final AuthAccountForm form,
 			final BindingResult result)
 		throws SQLException {
@@ -66,10 +69,8 @@ public class AuthAccountController {
 				
 				// TODO: log more info (login/password pair for example) (#59)
 				// TODO: sanitize all user's values (#60)
-				final String page    = request.getRequestURI();
-				final String ip      = request.getRemoteAddr();
-				final String referer = request.getHeader("referer");
-				final String agent   = request.getHeader("user-agent");
+				final String page = request.getRequestURI();
+				final String ip   = request.getRemoteAddr();
 				
 				Long uid = null;
 				final User user = (User)session.getAttribute("user");

src/main/java/ru/mystamps/web/controller/NotFoundErrorController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,13 +26,13 @@ public class NotFoundErrorController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String notFound(
 			final HttpServletRequest request,
-			final HttpSession session) {
+			final HttpSession session,
+			@RequestHeader("referer") final String referer,
+			@RequestHeader("user-agent") final String agent) {
 		
 		// TODO: sanitize all user's values (#60)
-		final String page    = (String)request.getAttribute("javax.servlet.error.request_uri");
-		final String ip      = request.getRemoteAddr();
-		final String referer = request.getHeader("referer");
-		final String agent   = request.getHeader("user-agent");
+		final String page = (String)request.getAttribute("javax.servlet.error.request_uri");
+		final String ip   = request.getRemoteAddr();
 		
 		Long uid = null;
 		final User user = (User)session.getAttribute("user");

commit 7e7596ffe1d688e9fe75521a1333ac1c4c5d2f2c

Fixed regression: IllegalStateException throws when one of headers missing.
Broken since cbbf655022772e9e575b8b9d250a82e7bcc11cc8 commit.

src/main/java/ru/mystamps/web/controller/AuthAccountController.java
@@ -51,8 +51,8 @@ public class AuthAccountController {
 	public String processInput(
 			final HttpServletRequest request,
 			final HttpSession session,
-			@RequestHeader("referer") final String referer,
-			@RequestHeader("user-agent") final String agent,
+			@RequestHeader(value = "referer", required = false) final String referer,
+			@RequestHeader(value = "user-agent", required = false) final String agent,
 			@Valid final AuthAccountForm form,
 			final BindingResult result)
 		throws SQLException {

src/main/java/ru/mystamps/web/controller/NotFoundErrorController.java
@@ -27,8 +27,8 @@ public class NotFoundErrorController {
 	public String notFound(
 			final HttpServletRequest request,
 			final HttpSession session,
-			@RequestHeader("referer") final String referer,
-			@RequestHeader("user-agent") final String agent) {
+			@RequestHeader(value = "referer", required = false) final String referer,
+			@RequestHeader(value = "user-agent", required = false) final String agent) {
 		
 		// TODO: sanitize all user's values (#60)
 		final String page = (String)request.getAttribute("javax.servlet.error.request_uri");