php-coder/mystamps

commit 4fb30a1d286db7b313e0d832ea50335322ee00c5

Generate random activation key.
Fixed #61

mystamps.sql
@@ -24,7 +24,10 @@ CREATE TABLE `users` (
 CREATE TABLE `users_activation` (
 	`email` VARCHAR(255) NOT NULL, -- see EMAIL_MAX_LENGTH in WEB-INF/web.xml
 	`registered_at` DATETIME NOT NULL,
-	`act_key` CHAR(10) NOT NULL, -- see ACT_KEY_LENGTH in WEB-INF/web.xml
+	-- see:
+	--- ACT_KEY_LENGTH in WEB-INF/web.xml
+	--- ru.mystamps.db.UsersActivation.generateActivationKey()
+	`act_key` CHAR(10) NOT NULL,
 	UNIQUE KEY(`act_key`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 

src/ru/mystamps/db/UsersActivation.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 
 import lombok.Cleanup;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 
 public class UsersActivation {
@@ -53,10 +54,13 @@ public class UsersActivation {
 	
 	/**
 	 * Generates activation key.
-	 * @todo implement it
+	 * @return string which contains numbers and letters in lower case
+	 *         in 10 characters length
 	 **/
 	private static String generateActivationKey() {
-		return "7777744444";
+		// Length of users_activation.act_key field in database equals
+		// to 10 characters (see mystamps.sql)
+		return RandomStringUtils.randomAlphanumeric(10).toLowerCase();
 	}
 	
 	
@@ -77,7 +81,8 @@ public class UsersActivation {
 			conn.prepareStatement(addRecordQuery);
 		
 		stat.setString(1, email);
-		stat.setString(2, generateActivationKey());
+		/// @todo: get rid of hardcoded act key
+		stat.setString(2, (email.equals("coder@rock.home") ? "7777744444" : generateActivationKey()));
 		stat.executeUpdate();
 	}
 	

commit b4ac1bc0811d5b2559c0cf4d55767a9cab15f0ef

mystamps.sql: fixed syntax errors.
Broken since 4fb30a1d286db7b313e0d832ea50335322ee00c5 commit.

mystamps.sql
@@ -42,8 +42,8 @@ CREATE TABLE `users_activation` (
 	`registered_at` DATETIME NOT NULL,
 	
 	-- see:
-	--- ACT_KEY_LENGTH in WEB-INF/web.xml
-	--- ru.mystamps.db.UsersActivation.generateActivationKey()
+	-- ACT_KEY_LENGTH in WEB-INF/web.xml
+	-- ru.mystamps.db.UsersActivation.generateActivationKey()
 	`act_key` CHAR(10) NOT NULL,
 	
 	UNIQUE KEY(`act_key`)