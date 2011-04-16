cbb16dbeedbc73c4fbdbf866014f1de356c00f8b
Slava Semushin <php-coder@altlinux.ru>
2011-04-16 22:10:18 +0700

    NotFoundErrorController: log possible exception from database.

--- src/main/java/ru/mystamps/web/controller/NotFoundErrorController.java
+++ src/main/java/ru/mystamps/web/controller/NotFoundErrorController.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -19,6 +21,7 @@ import static ru.mystamps.web.SiteMap.NOT_FOUND_PAGE_URL;
 @Controller
 @RequestMapping(NOT_FOUND_PAGE_URL)
 public class NotFoundErrorController {
+	private final Logger log = Logger.getRootLogger();
 	
 	@Autowired
 	private SuspiciousActivities act;
@@ -45,7 +48,7 @@ public class NotFoundErrorController {
 		} catch (final SQLException ex) {
 			// intentionally ignored:
 			// database error should not break showing of 404 page
-			// TODO: may be log such errors to file?
+			log.warn("Cannot log 404 error", ex);
 		}
 		
 		return "error/404";
