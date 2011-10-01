php-coder/mystamps

commit 876801d31681435b2d53ace98e411283beda39b2

Migrate XML-configuration to Java-configuration.
NOTE: config with DataSource included conditionally in each profile to not
include dependency from another profiles

Fixed #108

No functional changes.

src/main/java/ru/mystamps/web/config/DbConfig.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright (C) 2011 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ru.mystamps.web.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:spring/database.properties")
+public class DbConfig {
+	
+	@Value("${jpa.showSql}")
+	private String showSql;
+	
+	@Value("${jpa.dialectClassName}")
+	private String dialectClassName;
+	
+	@Value("${hibernate.formatSql}")
+	private String formatSql;
+	
+	@Value("${hibernate.hbm2ddl.auto}")
+	private String hbm2ddl;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public JpaVendorAdapter getJpaVendorAdapter() {
+		final HibernateJpaVendorAdapter jpaVendorAdapter =
+			new HibernateJpaVendorAdapter();
+		
+		jpaVendorAdapter.setDatabasePlatform(dialectClassName);
+		jpaVendorAdapter.setShowSql(Boolean.valueOf(showSql));
+		
+		return jpaVendorAdapter;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
+		final LocalContainerEntityManagerFactoryBean entityManagerFactory =
+			new LocalContainerEntityManagerFactoryBean();
+		
+		entityManagerFactory.setJpaVendorAdapter(getJpaVendorAdapter());
+		entityManagerFactory.setDataSource(dataSource);
+		
+		final Map<String, String> jpaProperties = new HashMap<String, String>();
+		jpaProperties.put("hibernate.format_sql", formatSql);
+		jpaProperties.put("hibernate.connection.charset", "UTF-8");
+		jpaProperties.put("hibernate.hbm2ddl.auto", hbm2ddl);
+		entityManagerFactory.setJpaPropertyMap(jpaProperties);
+		
+		return entityManagerFactory;
+	}
+	
+	@Bean
+	public PlatformTransactionManager getTransactionManager() {
+		final JpaTransactionManager transactionManager =
+			new JpaTransactionManager();
+		
+		transactionManager.setEntityManagerFactory(getEntityManagerFactory().getObject());
+		
+		return transactionManager;
+	}
+	
+}

src/main/java/ru/mystamps/web/config/DevDataSource.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2011 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ru.mystamps.web.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Profile("dev")
+@PropertySource("classpath:spring/database.properties")
+public class DevDataSource {
+	
+	@Value("${db.driverClassName}")
+	private String driverClassName;
+	
+	@Value("${db.url}")
+	private String url;
+	
+	@Value("${db.username}")
+	private String username;
+	
+	@Value("${db.password}")
+	private String password;
+	
+	@Bean(destroyMethod = "close")
+	public DataSource getDataSource() {
+		final BasicDataSource dataSource = new BasicDataSource();
+		
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		
+		return dataSource;
+	}
+	
+}
+

src/main/resources/spring/database.xml
@@ -1,71 +1,0 @@
-<beans xmlns="http://www.springframework.org/schema/beans"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns:context="http://www.springframework.org/schema/context"
-	xmlns:tx="http://www.springframework.org/schema/tx"
-	xmlns:jdbc="http://www.springframework.org/schema/jdbc"
-	xsi:schemaLocation="http://www.springframework.org/schema/beans
-	http://www.springframework.org/schema/beans/spring-beans-3.1.xsd
-	http://www.springframework.org/schema/context
-	http://www.springframework.org/schema/context/spring-context-3.1.xsd
-	http://www.springframework.org/schema/tx
-	http://www.springframework.org/schema/tx/spring-tx-3.1.xsd
-	http://www.springframework.org/schema/jdbc
-	http://www.springframework.org/schema/jdbc/spring-jdbc-3.1.xsd">
-	
-	<tx:annotation-driven />
-	
-	<context:property-placeholder location="classpath:spring/database.properties" />
-	
-	<bean id="jpaVendorAdapter"
-		class="org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter">
-		<property name="databasePlatform" value="${jpa.dialectClassName}" />
-		<property name="showSql" value="${jpa.showSql}" />
-	</bean>
-	
-	<bean id="entityManagerFactory"
-		class="org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean">
-		<property name="jpaVendorAdapter">
-			<ref local="jpaVendorAdapter" />
-		</property>
-		<property name="dataSource" ref="dataSource" />
-		<property name="jpaProperties">
-			<props>
-				<prop key="hibernate.format_sql">${hibernate.formatSql}</prop>
-				<prop key="hibernate.connection.charset">UTF-8</prop>
-				<prop key="hibernate.hbm2ddl.auto">${hibernate.hbm2ddl.auto}</prop>
-			</props>
-		</property>
-	</bean>
-	
-	<bean id="transactionManager"
-		class="org.springframework.orm.jpa.JpaTransactionManager">
-		<property name="entityManagerFactory">
-			<ref local="entityManagerFactory" />
-		</property>
-	</bean>
-	
-	<!-- enable injection of EntityManager to beans with @PersistenceContext annotation -->
-	<bean class="org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor" />
-	
-	<beans profile="dev">
-		<bean id="dataSource"
-			class="org.apache.commons.dbcp.BasicDataSource"
-			destroy-method="close">
-			<property name="driverClassName" value="${db.driverClassName}" />
-			<property name="url" value="${db.url}" />
-			<property name="username" value="${db.username}" />
-			<property name="password" value="${db.password}" />
-		</bean>
-	</beans>
-	
-	<beans profile="test">
-		<jdbc:embedded-database id="dataSource" type="HSQL">
-			<!-- hsql-scheme.sql generated by hibernate3-maven-plugin plugin -->
-			<jdbc:script location="classpath:hsql-scheme.sql" />
-			
-			<jdbc:script location="classpath:init-data.sql" />
-			<jdbc:script location="classpath:test-data.sql" />
-		</jdbc:embedded-database>
-	</beans>
-	
-</beans>

pom.xml
@@ -495,7 +495,6 @@
 					<groupId>commons-dbcp</groupId>
 					<artifactId>commons-dbcp</artifactId>
 					<version>1.4</version>
-					<scope>runtime</scope>
 				</dependency>
 				<dependency>
 					<groupId>mysql</groupId>
@@ -506,6 +505,16 @@
 			</dependencies>
 			<build>
 				<plugins>
+					
+					<plugin>
+						<groupId>org.apache.maven.plugins</groupId>
+						<artifactId>maven-compiler-plugin</artifactId>
+						<configuration>
+							<excludes>
+								<exclude>**/TestDataSource.java</exclude>
+							</excludes>
+						</configuration>
+					</plugin>
 					
 					<plugin>
 						<artifactId>maven-resources-plugin</artifactId>
@@ -562,7 +571,6 @@
 					<groupId>org.springframework</groupId>
 					<artifactId>spring-jdbc</artifactId>
 					<version>${spring.version}</version>
-					<scope>runtime</scope>
 				</dependency>
 				<dependency>
 					<groupId>org.hsqldb</groupId>
@@ -573,6 +581,16 @@
 			</dependencies>
 			<build>
 				<plugins>
+					
+					<plugin>
+						<groupId>org.apache.maven.plugins</groupId>
+						<artifactId>maven-compiler-plugin</artifactId>
+						<configuration>
+							<excludes>
+								<exclude>**/DevDataSource.java</exclude>
+							</excludes>
+						</configuration>
+					</plugin>
 					
 					<plugin>
 						<artifactId>maven-antrun-plugin</artifactId>

src/main/java/ru/mystamps/web/SiteMap.java
@@ -31,7 +31,7 @@ public final class SiteMap {
 	// defined at pom.xml (and used by functional tests only)
 	public static final String SITE_URL                  = "http://127.0.0.1:8081";
 	
-	// defined at src/main/resources/spring/DispatcherServletContext.xml
+	// defined at ru.mystamps.web.config.MvcConfig
 	public static final String INDEX_PAGE_URL            = "/";
 	public static final String MAINTENANCE_PAGE_URL      = "/site/maintenance";
 	public static final String SUCCESSFUL_REGISTRATION_PAGE_URL = "/successful/registration";
@@ -50,7 +50,7 @@ public final class SiteMap {
 	
 	public static final String ADD_COUNTRY_PAGE_URL      = "/country/add";
 	
-	// defined at src/main/resources/spring/DispatcherServletContext.xml
+	// defined at ru.mystamps.web.config.MvcConfig
 	public static final String RESTORE_PASSWORD_PAGE_URL = "/password/restore";
 	
 	// see also error-page definition at src/main/webapp/WEB-INF/web.xml

src/main/java/ru/mystamps/web/config/MvcConfig.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright (C) 2011 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ru.mystamps.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.ViewResolver;
+
+import static ru.mystamps.web.SiteMap.INDEX_PAGE_URL;
+import static ru.mystamps.web.SiteMap.MAINTENANCE_PAGE_URL;
+import static ru.mystamps.web.SiteMap.RESTORE_PASSWORD_PAGE_URL;
+import static ru.mystamps.web.SiteMap.SUCCESSFUL_ACTIVATION_PAGE_URL;
+import static ru.mystamps.web.SiteMap.SUCCESSFUL_REGISTRATION_PAGE_URL;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+	"ru.mystamps.web.controller",
+	"ru.mystamps.web.dao",
+	"ru.mystamps.web.service",
+	"ru.mystamps.web.validation"
+})
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Override
+	public void configureViewControllers(final ViewControllerConfigurer configurer) {
+		configurer.mapViewName(INDEX_PAGE_URL, "site/index");
+		configurer.mapViewName(MAINTENANCE_PAGE_URL, "site/maintenance");
+		configurer.mapViewName(RESTORE_PASSWORD_PAGE_URL, "password/restore");
+		configurer.mapViewName(SUCCESSFUL_ACTIVATION_PAGE_URL, "account/activation_successful");
+		configurer.mapViewName(SUCCESSFUL_REGISTRATION_PAGE_URL, "account/activation_sent");
+	}
+	
+	@Override
+	public void configureResourceHandling(final ResourceConfigurer configurer) {
+		configurer.addPathMapping("/static/**").addResourceLocation("/WEB-INF/static/*");
+	}
+	
+	@Bean
+	public ViewResolver getViewResolver() {
+		final InternalResourceViewResolver viewResolver =
+			new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean(name = "messageSource")
+	MessageSource getMessageSource() {
+		final ReloadableResourceBundleMessageSource messageSource =
+			new ReloadableResourceBundleMessageSource();
+		
+		messageSource.setBasename("classpath:ru/mystamps/i18n/Messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		
+		return messageSource;
+	}
+	
+}

src/main/java/ru/mystamps/web/config/TestDataSource.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2011 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package ru.mystamps.web.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("test")
+public class TestDataSource {
+	
+	@Bean
+	public DataSource getDataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setType(EmbeddedDatabaseType.HSQL)
+			.addScript("classpath:hsql-scheme.sql")
+			.addScript("classpath:init-data.sql")
+			.addScript("classpath:test-data.sql")
+			.build();
+	}
+}
+

src/main/resources/spring/DispatcherServletContext.xml
@@ -1,40 +1,0 @@
-<beans xmlns="http://www.springframework.org/schema/beans"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns:mvc="http://www.springframework.org/schema/mvc"
-	xmlns:context="http://www.springframework.org/schema/context"
-	xsi:schemaLocation="http://www.springframework.org/schema/beans
-	http://www.springframework.org/schema/beans/spring-beans-3.1.xsd
-	http://www.springframework.org/schema/mvc
-	http://www.springframework.org/schema/mvc/spring-mvc-3.1.xsd
-	http://www.springframework.org/schema/context
-	http://www.springframework.org/schema/context/spring-context-3.1.xsd">
-	
-	<context:component-scan
-		base-package="ru.mystamps.web.controller,ru.mystamps.web.dao,ru.mystamps.web.service,ru.mystamps.web.validation" />
-	
-	<bean id="viewResolver"
-		class="org.springframework.web.servlet.view.InternalResourceViewResolver" >
-		<property name="prefix" value="/WEB-INF/pages/" />
-		<property name="suffix" value=".jsp" />
-	</bean>
-	
-	<bean id="messageSource"
-		class="org.springframework.context.support.ReloadableResourceBundleMessageSource">
-		<property name="basename" value="classpath:ru/mystamps/i18n/Messages" />
-		<property name="defaultEncoding" value="UTF-8" />
-	</bean>
-	
-	<mvc:annotation-driven />
-	<mvc:default-servlet-handler />
-	<mvc:resources mapping="/static/**" location="/WEB-INF/static/*" />
-	
-	<!-- see also src/ru/mystamps/web/SiteMap.java -->
-	<mvc:view-controller path="/" view-name="site/index" />
-	<mvc:view-controller path="/site/maintenance" view-name="site/maintenance" />
-	<mvc:view-controller path="/password/restore" view-name="password/restore" />
-	<mvc:view-controller path="/successful/registration" view-name="account/activation_sent" />
-	<mvc:view-controller path="/successful/activation" view-name="account/activation_successful" />
-	
-	<import resource="database.xml" />
-	
-</beans>

commit 5e5afdb6c196ae1bc7375210ccc831efb8955a7e

Unbreak application under dev profile.
@PropertySource don't substitute values from property file, so replace
it to using @ImportResource with <context:property-placeholder />

Looks like I broke it in 876801d31681435b2d53ace98e411283beda39b2
commit when migrated to Java configs. Don't fully understand why it
worked under test profile.

src/main/java/ru/mystamps/web/config/DbConfig.java
@@ -27,7 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -37,7 +37,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:spring/database.properties")
+@ImportResource("classpath:spring/database.xml")
 public class DbConfig {
 	
 	@Value("${jpa.showSql}")

src/main/java/ru/mystamps/web/config/DevDataSource.java
@@ -25,12 +25,12 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Profile("dev")
-@PropertySource("classpath:spring/database.properties")
+@ImportResource("classpath:spring/database.xml")
 public class DevDataSource {
 	
 	@Value("${db.driverClassName}")

src/main/resources/spring/database.xml
@@ -1,0 +1,11 @@
+<beans xmlns="http://www.springframework.org/schema/beans"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:context="http://www.springframework.org/schema/context"
+	xsi:schemaLocation="http://www.springframework.org/schema/beans
+	http://www.springframework.org/schema/beans/spring-beans-3.1.xsd
+	http://www.springframework.org/schema/context
+	http://www.springframework.org/schema/context/spring-context-3.1.xsd">
+	
+	<context:property-placeholder location="classpath:spring/database.properties" />
+	
+</beans>