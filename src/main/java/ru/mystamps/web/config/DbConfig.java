556d38c70902fb67d09c7c5fc6b65d34073845eb
Slava Semushin <php-coder@altlinux.ru>
2012-01-11 00:38:05 +0700

    Revert "Get rid of persistence.xml usage."
    This reverts commit b35ae46892e6c7d13a6b6e4120ddc410b7df8d4a.
    
    Reverted because it breaks `mvn -P test hibernate3:hbm2ddl`:
    INFO org.hibernate.ejb.Ejb3Configuration - HHH000318: Could not find any META-INF/persistence.xml file in the classpath

--- src/main/java/ru/mystamps/web/config/DbConfig.java
+++ src/main/java/ru/mystamps/web/config/DbConfig.java
@@ -75,8 +75,6 @@ public class DbConfig {
 		entityManagerFactory.setJpaVendorAdapter(getJpaVendorAdapter());
 		entityManagerFactory.setDataSource(dataSource);
 		
-		entityManagerFactory.setPackagesToScan(new String[] { "ru.mystamps.web.entity" });
-		
 		final Map<String, String> jpaProperties = new HashMap<String, String>();
 		jpaProperties.put("hibernate.format_sql", formatSql);
 		jpaProperties.put("hibernate.connection.charset", "UTF-8");
