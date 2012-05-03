php-coder/mystamps

commit 00abf9425cc2a21f0f931952fceba08897af633d

Ported validation to using JSR-303 annotations.
Changes in dependencies:
- commons-validator and oro was removed (due to hibernate-validator usage)
- commons-beanutils was added

Fixed #86

src/test/java/ru/mystamps/web/tests/cases/WhenUserActivateAccount.java
@@ -149,7 +149,7 @@ public class WhenUserActivateAccount extends WhenUserAtAnyPageWithForm<ActivateA
 		
 		assertThat(page)
 			.field("login")
-			.hasError(tr("login.exists"));
+			.hasError(tr("ru.mystamps.web.validation.jsr303.UniqueLogin.message"));
 	}
 	
 	@Test
@@ -275,7 +275,7 @@ public class WhenUserActivateAccount extends WhenUserAtAnyPageWithForm<ActivateA
 		
 		assertThat(page)
 			.field("activationKey")
-			.hasError(tr("key.not-exists"));
+			.hasError(tr("ru.mystamps.web.validation.jsr303.ExistingActivationKey.message"));
 	}
 	
 	@Test

pom.xml
@@ -127,9 +127,9 @@
 		</dependency>
 		
 		<dependency>
-			<groupId>commons-validator</groupId>
-			<artifactId>commons-validator</artifactId>
-			<version>1.3.1</version>
+			<groupId>commons-beanutils</groupId>
+			<artifactId>commons-beanutils</artifactId>
+			<version>1.8.3</version>
 		</dependency>
 		
 		<dependency>
@@ -231,13 +231,6 @@
 			<artifactId>slf4j-log4j12</artifactId>
 			<version>${slf4j.version}</version>
 			<scope>runtime</scope>
-		</dependency>
-		
-		<!-- commons-validator use it to validating e-mails -->
-		<dependency>
-			<groupId>oro</groupId>
-			<artifactId>oro</artifactId>
-			<version>2.0.8</version>
 		</dependency>
 		
 		<dependency>

src/main/config/checkstyle-suppressions.xml
@@ -18,4 +18,12 @@
 	<suppress checks="AbstractClassName" files="ru.mystamps.web.tests.cases.WhenUserAtAnyPage" />
 	<suppress checks="AbstractClassName" files="ru.mystamps.web.tests.cases.WhenUserAtAnyPageWithForm" />
 	
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.Email" />
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.ExistingActivationKey" />
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.FieldsMatch" />
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.FieldsMismatch" />
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.UniqueCountryName" />
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.UniqueLogin" />
+	<suppress checks="WhitespaceAround" files="ru.mystamps.web.validation.jsr303.ValidCredentials" />
+	
 </suppressions>

src/main/config/checkstyle.xml
@@ -122,7 +122,7 @@
         <!-- See http://checkstyle.sf.net/config_import.html -->
         <module name="AvoidStarImport"/>
         <module name="IllegalImport"> <!-- defaults to sun.* packages -->
-            <property name="illegalPkgs" value="java.sql, org.hibernate, org.apache.log4j"/>
+            <property name="illegalPkgs" value="java.sql, org.apache.log4j"/>
         </module>
         <module name="RedundantImport"/>
         <module name="UnusedImports"/>

src/main/java/ru/mystamps/web/config/MvcConfig.java
@@ -42,8 +42,7 @@ import static ru.mystamps.web.SiteMap.SUCCESSFUL_REGISTRATION_PAGE_URL;
 @ComponentScan(basePackages = {
 	"ru.mystamps.web.controller",
 	"ru.mystamps.web.dao",
-	"ru.mystamps.web.service",
-	"ru.mystamps.web.validation"
+	"ru.mystamps.web.service"
 })
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	

src/main/java/ru/mystamps/web/controller/ActivateAccountController.java
@@ -21,7 +21,6 @@ package ru.mystamps.web.controller;
 import java.util.Map;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
@@ -31,10 +30,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 import ru.mystamps.web.model.ActivateAccountForm;
+import ru.mystamps.web.model.ActivateAccountForm.LoginChecks;
+import ru.mystamps.web.model.ActivateAccountForm.NameChecks;
+import ru.mystamps.web.model.ActivateAccountForm.PasswordChecks;
+import ru.mystamps.web.model.ActivateAccountForm.PasswordConfirmationChecks;
+import ru.mystamps.web.model.ActivateAccountForm.ActKeyChecks;
+import ru.mystamps.web.model.ActivateAccountForm.FormChecks;
 import ru.mystamps.web.service.UserService;
-import ru.mystamps.web.validation.ActivateAccountValidator;
 
 import static ru.mystamps.web.SiteMap.ACTIVATE_ACCOUNT_PAGE_URL;
 import static ru.mystamps.web.SiteMap.ACTIVATE_ACCOUNT_PAGE_WITH_KEY_URL;
@@ -44,21 +49,15 @@ import static ru.mystamps.web.SiteMap.SUCCESSFUL_ACTIVATION_PAGE_URL;
 public class ActivateAccountController {
 	
 	private final UserService userService;
-	private final ActivateAccountValidator activateAccountValidator;
 	
 	@Inject
-	ActivateAccountController(
-		final UserService userService,
-		final ActivateAccountValidator activateAccountValidator) {
-		
+	ActivateAccountController(final UserService userService) {
 		this.userService = userService;
-		this.activateAccountValidator = activateAccountValidator;
 	}
 	
 	@InitBinder
 	protected void initBinder(final WebDataBinder binder) {
-		binder.setValidator(activateAccountValidator);
-		binder.registerCustomEditor(String.class, "name", new StringTrimmerEditor(false));
+		binder.registerCustomEditor(String.class, "name", new StringTrimmerEditor(true));
 	}
 	
 	@RequestMapping(value = ACTIVATE_ACCOUNT_PAGE_URL, method = RequestMethod.GET)
@@ -78,8 +77,10 @@ public class ActivateAccountController {
 	
 	@RequestMapping(value = ACTIVATE_ACCOUNT_PAGE_URL, method = RequestMethod.POST)
 	public String processInput(
-			@Valid final ActivateAccountForm form,
-			final BindingResult result) {
+		@Validated({
+			LoginChecks.class, NameChecks.class, PasswordChecks.class,
+			PasswordConfirmationChecks.class, ActKeyChecks.class, FormChecks.class
+		}) final ActivateAccountForm form, final BindingResult result) {
 		
 		if (result.hasErrors()) {
 			return null;

src/main/java/ru/mystamps/web/controller/AddCountryController.java
@@ -31,7 +31,6 @@ import org.springframework.validation.BindingResult;
 
 import ru.mystamps.web.model.AddCountryForm;
 import ru.mystamps.web.service.CountryService;
-import ru.mystamps.web.validation.AddCountryValidator;
 
 import static ru.mystamps.web.SiteMap.ADD_COUNTRY_PAGE_URL;
 import static ru.mystamps.web.SiteMap.INFO_COUNTRY_PAGE_URL;
@@ -40,20 +39,15 @@ import static ru.mystamps.web.SiteMap.INFO_COUNTRY_PAGE_URL;
 @RequestMapping(ADD_COUNTRY_PAGE_URL)
 public class AddCountryController {
 	
-	private final AddCountryValidator addCountryValidator;
 	private final CountryService countryService;
 	
 	@Inject
-	AddCountryController(
-			final AddCountryValidator addCountryValidator,
-			final CountryService countryService) {
-		this.addCountryValidator = addCountryValidator;
+	AddCountryController(final CountryService countryService) {
 		this.countryService = countryService;
 	}
 	
 	@InitBinder
 	protected void initBinder(final WebDataBinder binder) {
-		binder.setValidator(addCountryValidator);
 		binder.registerCustomEditor(String.class, "country", new StringTrimmerEditor(false));
 	}
 	

src/main/java/ru/mystamps/web/controller/AuthAccountController.java
@@ -21,21 +21,21 @@ package ru.mystamps.web.controller;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 import ru.mystamps.web.entity.User;
 import ru.mystamps.web.model.AuthAccountForm;
+import ru.mystamps.web.model.AuthAccountForm.FormChecks;
+import ru.mystamps.web.model.AuthAccountForm.LoginChecks;
+import ru.mystamps.web.model.AuthAccountForm.PasswordChecks;
 import ru.mystamps.web.service.SiteService;
 import ru.mystamps.web.service.UserService;
-import ru.mystamps.web.validation.AuthAccountValidator;
 
 import static ru.mystamps.web.SiteMap.INDEX_PAGE_URL;
 import static ru.mystamps.web.SiteMap.AUTHENTICATION_PAGE_URL;
@@ -46,22 +46,11 @@ public class AuthAccountController {
 	
 	private final UserService userService;
 	private final SiteService siteService;
-	private final AuthAccountValidator authAccountValidator;
 	
 	@Inject
-	AuthAccountController(
-		final UserService userService,
-		final SiteService siteService,
-		final AuthAccountValidator authAccountValidator) {
-		
+	AuthAccountController(final UserService userService, final SiteService siteService) {
 		this.userService = userService;
 		this.siteService = siteService;
-		this.authAccountValidator = authAccountValidator;
-	}
-	
-	@InitBinder
-	protected void initAuthBinder(final WebDataBinder binder) {
-		binder.setValidator(authAccountValidator);
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
@@ -75,7 +64,11 @@ public class AuthAccountController {
 			final HttpSession session,
 			@RequestHeader(value = "referer", required = false) final String referer,
 			@RequestHeader(value = "user-agent", required = false) final String agent,
-			@Valid final AuthAccountForm form,
+			@Validated({
+				LoginChecks.class,
+				PasswordChecks.class,
+				FormChecks.class
+			}) final AuthAccountForm form,
 			final BindingResult result) {
 		
 		if (result.hasErrors()) {
@@ -83,10 +76,9 @@ public class AuthAccountController {
 			// When user provides wrong login/password pair than
 			// validation mechanism add error to form. Check this to
 			// handle situation with wrong credentials.
-			// @see AuthAccountValidator::validateLoginPasswordPair()
 			if (result.hasGlobalErrors()
 					&& result.getGlobalError() != null
-					&& result.getGlobalError().getCode().equals("login.password.invalid")) {
+					&& "ValidCredentials".equals(result.getGlobalError().getCode())) {
 				
 				// TODO: log more info (login for example) (#59)
 				// TODO: sanitize all user's values (#60)

commit ac00e21cb2f4bc91426d8b8f43fcef5046f0d08c

account/activate: unbreak activation with empty name.
Broken since 00abf9425cc2a21f0f931952fceba08897af633d commit where I allow
transformation from empty string to null for field with name.

NOTE: I removed @CacheLookup annotation for body element to prevent
StaleElementReferenceException when we moved from current page and trying to
get text at another page.

src/main/java/ru/mystamps/web/service/UserService.java
@@ -33,6 +33,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 import ru.mystamps.web.entity.User;
 import ru.mystamps.web.entity.UsersActivation;
@@ -77,12 +78,11 @@ public class UserService {
 		
 		checkArgument(login != null, "Login should be non null");
 		checkArgument(password != null, "Password should be non null");
-		checkArgument(name != null, "Name should be non null");
 		checkArgument(activationKey != null, "Activation key should be non null");
 		
 		// use login as name if name is not provided
 		final String finalName;
-		if ("".equals(name)) {
+		if (isNullOrEmpty(name)) {
 			finalName = login;
 		} else {
 			finalName = name;

src/env/test/WEB-INF/classes/spring/test-credentials.properties
@@ -17,6 +17,7 @@ invalid_user_password = test
 
 # this user should be just registered but not activated
 not_activated_user_act_key = 7777744444
+not_activated_user2_act_key = 4444477777
 
 # this country should always exist
 valid_country_name = Italy

src/env/test/WEB-INF/classes/test-data.sql
@@ -19,6 +19,13 @@ VALUES(
 	NOW()
 );
 
+INSERT INTO users_activation(act_key, email, created_at)
+VALUES(
+	'@not_activated_user2_act_key@',
+	'test2@example.org',
+	NOW()
+);
+
 INSERT INTO countries(name, created_at)
 VALUES(
 	'@valid_country_name@',

src/test/java/ru/mystamps/web/service/UserServiceTest.java
@@ -200,11 +200,6 @@ public class UserServiceTest {
 		verify(usersActivationDao, never()).delete(any(UsersActivation.class));
 	}
 	
-	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void registerUserShouldThrowExceptionWhenNameIsNull() {
-		service.registerUser(TEST_LOGIN, TEST_PASSWORD, null, TEST_ACTIVATION_KEY);
-	}
-	
 	@Test
 	public void registerUserShouldPassNameToDao() {
 		when(usersActivationDao.findByActivationKey(anyString())).thenReturn(getUsersActivation());
@@ -214,6 +209,17 @@ public class UserServiceTest {
 		verify(userDao).save(userCaptor.capture());
 		
 		assertThat(userCaptor.getValue().getName()).isEqualTo(TEST_NAME);
+	}
+	
+	@Test
+	public void registerUserShouldPassLoginInsteadOfNameWhenNameIsNull() {
+		when(usersActivationDao.findByActivationKey(anyString())).thenReturn(getUsersActivation());
+		
+		service.registerUser(TEST_LOGIN, TEST_PASSWORD, null, TEST_ACTIVATION_KEY);
+		
+		verify(userDao).save(userCaptor.capture());
+		
+		assertThat(userCaptor.getValue().getName()).isEqualTo(TEST_LOGIN);
 	}
 	
 	@Test

src/test/java/ru/mystamps/web/tests/cases/WhenUserActivateAccount.java
@@ -51,6 +51,9 @@ public class WhenUserActivateAccount extends WhenUserAtAnyPageWithForm<ActivateA
 	@Value("#{test.not_activated_user_act_key}")
 	private String notActivatedUserActKey;
 	
+	@Value("#{test.not_activated_user2_act_key}")
+	private String secondNotActivatedUserActKey;
+	
 	public WhenUserActivateAccount() {
 		super(ActivateAccountPage.class);
 		hasTitle(tr("t_activation_title"));
@@ -291,6 +294,23 @@ public class WhenUserActivateAccount extends WhenUserAtAnyPageWithForm<ActivateA
 		assertThat(page.existsLinkTo(AUTHENTICATION_PAGE_URL))
 			.overridingErrorMessage("should exists link to authentication page")
 			.isTrue();
+	}
+	
+	@Test(groups = "logic", dependsOnGroups = {
+		"std", "invalid", "valid", "misc"
+	})
+	public void activationShouldPassWhenUserProvidedEmptyName() {
+		page.activateAccount(
+			"2nd-test-login",
+			"",
+			"test-password",
+			"test-password",
+			secondNotActivatedUserActKey
+		);
+		
+		assertThat(page.getCurrentUrl()).isEqualTo(SUCCESSFUL_ACTIVATION_PAGE_URL);
+		
+		assertThat(page.textPresent(stripHtmlTags(tr("t_activation_successful")))).isTrue();
 	}
 	
 	@DataProvider(name = "validNames")

src/test/java/ru/mystamps/web/tests/page/AbstractPage.java
@@ -27,7 +27,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -49,7 +48,6 @@ public abstract class AbstractPage {
 	private final String pageUrl;
 	
 	@FindBy(tagName = "body")
-	@CacheLookup
 	private WebElement body;
 	
 	public void open() {