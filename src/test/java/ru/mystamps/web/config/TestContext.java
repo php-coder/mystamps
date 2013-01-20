4fca9df0b3117fa6edcf8871053b3cc2fbe98bfc
Slava Semushin <php-coder@altlinux.ru>
2013-01-20 19:02:59 +0400

    proxy-target-class="true" no needed anymore.
    After switching to use interfaces for services (since
    e8dbe672d247a64208e4694fd9db2fc868158009 commit) we no need to use
    <aop:config proxy-target-class="true" />, so I removed it with all
    dependencies.
    
    No functional changes.

--- src/test/java/ru/mystamps/web/config/TestContext.java
+++ src/test/java/ru/mystamps/web/config/TestContext.java
@@ -19,12 +19,10 @@ package ru.mystamps.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-@ImportResource("classpath:spring/aop.xml")
 public class TestContext {
 	
 	@Bean
