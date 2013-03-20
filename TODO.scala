45420aedb1c2c9992e8aef6298fd9f776b9fd317
Slava Semushin <php-coder@altlinux.ru>
2013-03-21 00:13:44 +0400

    TODO: +1

--- TODO.scala
+++ TODO.scala
@@ -16,3 +16,7 @@
 * использовать require:
   - Validate.isTrue(dto != null, "DTO must be non null");
   + require(dto != null, "DTO must be non null")
+
+* использовать трейт Versionable/Identifiable/Auditable
+  (created/updated) для сущностей
+
