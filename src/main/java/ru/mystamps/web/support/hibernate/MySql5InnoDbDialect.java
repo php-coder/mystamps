919b241ed0020fd9e606cd9be5d096ebac48878f
Slava Semushin <php-coder@altlinux.ru>
2013-03-24 13:56:36 +0400

    Added customized version of MySQL5InnoDBDialect to fix deployment to MySQL.
    Fixed following exception:
    org.hibernate.HibernateException: Wrong column type in mystamps.series for column perforated. Found: bit, expected: boolean
    
    @see http://stackoverflow.com/questions/8667965/found-bit-expected-boolean-after-hibernate-4-upgrade
    @see https://hibernate.atlassian.net/browse/HHH-6935

---
+++ src/main/java/ru/mystamps/web/support/hibernate/MySql5InnoDbDialect.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2009-2013 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package ru.mystamps.web.support.hibernate;
+
+import java.sql.Types;
+
+import org.hibernate.dialect.MySQL5InnoDBDialect;
+
+/**
+ * Customized version of MySQL5InnoDBDialect.
+ *
+ * Allows to use bit type in database for boolean members.
+ * @see http://stackoverflow.com/q/8667965/found-bit-expected-boolean-after-hibernate-4-upgrade
+ * @see https://hibernate.atlassian.net/browse/HHH-6935
+ **/
+public class MySql5InnoDbDialect extends MySQL5InnoDBDialect {
+	
+	public MySql5InnoDbDialect() {
+		super();
+		registerColumnType(Types.BOOLEAN, "bit");
+	}
+	
+}
+
