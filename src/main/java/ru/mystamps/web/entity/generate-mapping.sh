5bb34e52d7c192023f722b01fee73dbd916ea74c
Slava Semushin <php-coder@altlinux.ru>
2013-06-14 23:38:23 +0400

    Generate orm.xml

--- src/main/java/ru/mystamps/web/entity/generate-mapping.sh
+++ src/main/java/ru/mystamps/web/entity/generate-mapping.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . -type f -name 'Price.java' |
+find . -type f -name '*.java' |
 xargs egrep -l '@(Entity|Embeddable)' |
 while read FILE; do
 	TABLE=$(fgrep @Table "$FILE" | sed 's|.*name = "\([^"]\+\)".*|\1|')
@@ -10,12 +10,12 @@ while read FILE; do
 	EMBEDDABLE=$(fgrep '@Embeddable' "$FILE")
 	
 	if [ -z "$EMBEDDABLE" ]; then
-		echo "<entity name=\"$SHORT_CLASS_NAME\" class=\"$FULL_CLASS_NAME\" access=\"FIELD\">"
+		echo "\t<entity name=\"$SHORT_CLASS_NAME\" class=\"$FULL_CLASS_NAME\" access=\"FIELD\">"
 	else
-		echo "<embeddable class=\"$FULL_CLASS_NAME\" access=\"FIELD\">"
+		echo "\t<embeddable class=\"$FULL_CLASS_NAME\" access=\"FIELD\">"
 	fi
 	
-	echo "\t<attributes>"
+	echo "\t\t<attributes>"
 	
 	fgrep private "$FILE" | sed 's|^[\t ]\+private ||;s|;.*$||' |
 		while read FIELD_TYPE FIELD_NAME; do
@@ -24,25 +24,25 @@ while read FILE; do
 			# TODO: use simple <id name="id" /> ?
 			IS_ID=$(echo "$ANNOTATIONS" | fgrep '@Id')
 			if [ -n "$IS_ID" ]; then
-				echo "\t\t<id name=\"$FIELD_NAME\">"
-				echo "\t\t\t<column name=\"$FIELD_NAME\" />"
+				echo "\t\t\t<id name=\"$FIELD_NAME\">"
+				echo "\t\t\t\t<column name=\"$FIELD_NAME\" />"
 				if [ -n "$(echo "$ANNOTATIONS | fgrep '@GeneratedValue'")" ]; then
-					echo "\t\t\t<generated-value />"
+					echo "\t\t\t\t<generated-value />"
 				fi
-				echo "\t\t</id>"
+				echo "\t\t\t</id>"
 				continue
 			fi
 			
 			COLUMN=$(echo "$ANNOTATIONS" | fgrep '@Column')
 			if [ -n "$COLUMN" ]; then
-				echo "\t\t<basic name=\"$FIELD_NAME\">"
+				echo "\t\t\t<basic name=\"$FIELD_NAME\">"
 				COLUMN_NAME=$(echo "$COLUMN" | sed -n 's|.*name = "\([^"]\+\)".*|\1|p')
 				if [ -n "$COLUMN_NAME" ]; then
 					NAME=$COLUMN_NAME
 				else
 					NAME=$FIELD_NAME
 				fi
-				echo -n "\t\t\t<column name=\"$NAME\""
+				echo -n "\t\t\t\t<column name=\"$NAME\""
 				LENGTH=$(echo "$COLUMN" | sed -n 's|.*length = \([^,)]\+\).*|\1|p')
 				if [ -n "$LENGTH" ]; then
 					if [ -z "$(echo "$LENGTH" | grep '^[0-9]\+$')" ]; then
@@ -68,15 +68,15 @@ while read FILE; do
 				ENUM="$(echo "$ANNOTATIONS" | fgrep '@Enumerated')"
 				if [ -n "$ENUM" ]; then
 					ENUM_TYPE=$(echo "$ENUM" | sed 's|.*@Enumerated(\([^)]\+\)).*|\1|' | sed 's|EnumType\.||')
-					echo "\t\t\t<enumerated>$ENUM_TYPE</enumerated>"
+					echo "\t\t\t\t<enumerated>$ENUM_TYPE</enumerated>"
 				fi
-				echo "\t\t</basic>"
+				echo "\t\t\t</basic>"
 				continue
 			fi
 			
 			IS_EMBEDDED=$(echo "$ANNOTATIONS" | fgrep '@Embedded')
 			if [ -n "$IS_EMBEDDED" ]; then
-				echo "\t\t<embedded name=\"$FIELD_NAME\" />"
+				echo "\t\t\t<embedded name=\"$FIELD_NAME\" />"
 				continue
 			fi
 			
@@ -84,12 +84,12 @@ while read FILE; do
 			
 		done
 	
-	echo "\t</attributes>"
+	echo "\t\t</attributes>"
 	
 	if [ -z "$EMBEDDABLE" ]; then
-		echo "</entity>"
+		echo "\t</entity>"
 	else
-		echo "</embeddable>"
+		echo "\t</embeddable>"
 	fi
 	
 done
