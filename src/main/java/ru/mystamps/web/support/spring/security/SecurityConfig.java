php-coder/mystamps

commit 7c2b68f8bbc61f688aeca40c17175ca2f5763e99

Activate CSRF protection.
Now POST method is used for logging out from site.

@see http://docs.spring.io/spring-security/site/docs/3.2.x/reference/htmlsingle/#csrf

Fix #25

src/main/config/checkstyle-suppressions.xml
@@ -5,11 +5,12 @@
 
 <suppressions>
 	
+	<suppress checks="LineLength" files="AbstractPage.java" lines="44,45" />
 	<suppress checks="LineLength" files="AbstractPageWithForm.java" lines="27,28,33" />
 	<suppress checks="LineLength" files="Form.java" lines="31,32,37" />
 	<suppress checks="LineLength" files="Url.java" lines="73" />
 	<suppress checks="LineLength" files="ErrorController.java" lines="73" />
-	<suppress checks="LineLength" files="SecurityConfig.java" lines="35,36,40" />
+	<suppress checks="LineLength" files="SecurityConfig.java" lines="35,36,39,40" />
 	
 	<!-- false positives due to Lombok usage -->
 	<suppress checks="HideUtilityClassConstructor" files="ru.mystamps.web.model" />

src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java
@@ -36,15 +36,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import ru.mystamps.web.config.ServicesConfig;
 import ru.mystamps.web.Url;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
@@ -94,9 +94,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.rememberMe()
 				// TODO: GH #27
-				.disable()
-			.csrf()
-				// TODO: GH #25
 				.disable()
 			.headers()
 				// TODO

src/main/webapp/WEB-INF/static/styles/main.css
@@ -58,3 +58,12 @@ label {
 .top-indent {
 	padding-top: 20px;
 }
+
+.no-margin {
+	margin: 0px;
+}
+
+.no-padding {
+	padding: 0px;
+}
+

src/main/webapp/WEB-INF/views/account/activate.html
@@ -37,8 +37,9 @@
 					</li>
 					<!--/*/
 					<li sec:authorize="isAuthenticated()">
-						<i class="icon-share"></i>
-						<a href="../site/index.html" th:href="@{${LOGOUT_PAGE}}" th:text="#{t_logout}">Sign out</a>
+						<form id="LogoutForm" method="get" action="../site/index.html" class="no-margin" th:method="post" th:action="@{${LOGOUT_PAGE}}">
+							<i class="icon-share"></i>&nbsp;<input type="submit" value="Sign out" class="btn btn-link no-padding" th:value="#{t_logout}" />
+						</form>
 					</li>
 					/*/-->
 					<li sec:authorize="isAnonymous()">

src/main/webapp/WEB-INF/views/account/auth.html
@@ -37,8 +37,9 @@
 					</li>
 					<!--/*/
 					<li sec:authorize="isAuthenticated()">
-						<i class="icon-share"></i>
-						<a href="../site/index.html" th:href="@{${LOGOUT_PAGE}}" th:text="#{t_logout}">Sign out</a>
+						<form id="LogoutForm" method="get" action="../site/index.html" class="no-margin" th:method="post" th:action="@{${LOGOUT_PAGE}}">
+							<i class="icon-share"></i>&nbsp;<input type="submit" value="Sign out" class="btn btn-link no-padding" th:value="#{t_logout}" />
+						</form>
 					</li>
 					/*/-->
 					<li sec:authorize="isAnonymous()">

src/main/webapp/WEB-INF/views/account/register.html
@@ -37,8 +37,9 @@
 					</li>
 					<!--/*/
 					<li sec:authorize="isAuthenticated()">
-						<i class="icon-share"></i>
-						<a href="../site/index.html" th:href="@{${LOGOUT_PAGE}}" th:text="#{t_logout}">Sign out</a>
+						<form id="LogoutForm" method="get" action="../site/index.html" class="no-margin" th:method="post" th:action="@{${LOGOUT_PAGE}}">
+							<i class="icon-share"></i>&nbsp;<input type="submit" value="Sign out" class="btn btn-link no-padding" th:value="#{t_logout}" />
+						</form>
 					</li>
 					/*/-->
 					<li sec:authorize="isAnonymous()">

src/main/webapp/WEB-INF/views/category/add.html
@@ -36,8 +36,9 @@
 					</li>
 					/*/-->
 					<li sec:authorize="isAuthenticated()">
-						<i class="icon-share"></i>
-						<a href="../site/index.html" th:href="@{${LOGOUT_PAGE}}" th:text="#{t_logout}">Sign out</a>
+						<form id="LogoutForm" method="get" action="../site/index.html" class="no-margin" th:method="post" th:action="@{${LOGOUT_PAGE}}">
+							<i class="icon-share"></i>&nbsp;<input type="submit" value="Sign out" class="btn btn-link no-padding" th:value="#{t_logout}" />
+						</form>
 					</li>
 					<!--/*/
 					<li sec:authorize="isAnonymous()">

src/main/webapp/WEB-INF/views/category/info.html
@@ -37,8 +37,9 @@
 					</li>
 					<!--/*/
 					<li sec:authorize="isAuthenticated()">
-						<i class="icon-share"></i>
-						<a href="../site/index.html" th:href="@{${LOGOUT_PAGE}}" th:text="#{t_logout}">Sign out</a>
+						<form id="LogoutForm" method="get" action="../site/index.html" class="no-margin" th:method="post" th:action="@{${LOGOUT_PAGE}}">
+							<i class="icon-share"></i>&nbsp;<input type="submit" value="Sign out" class="btn btn-link no-padding" th:value="#{t_logout}" />
+						</form>
 					</li>
 					/*/-->
 					<li sec:authorize="isAnonymous()">

commit e7c4fa39807ff14a00d303b7355f0e474e87b6a5

Don't check CSRF token for 3rd party servlets (like Togglz and H2 console).
Broken since 7c2b68f8bbc61f688aeca40c17175ca2f5763e99 commit (GH #25) where CSRF protection was enabled.

src/main/config/checkstyle-suppressions.xml
@@ -10,7 +10,7 @@
 	<suppress checks="LineLength" files="Form.java" lines="31,32,37" />
 	<suppress checks="LineLength" files="Url.java" lines="73" />
 	<suppress checks="LineLength" files="ErrorController.java" lines="73" />
-	<suppress checks="LineLength" files="SecurityConfig.java" lines="35,36,39,40" />
+	<suppress checks="LineLength" files="SecurityConfig.java" lines="36,37,40,41" />
 	
 	<!-- false positives due to Lombok usage -->
 	<suppress checks="HideUtilityClassConstructor" files="ru.mystamps.web.model" />

src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java
@@ -18,6 +18,7 @@
 package ru.mystamps.web.support.spring.security;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +40,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import ru.mystamps.web.config.ServicesConfig;
 import ru.mystamps.web.Url;
@@ -92,6 +94,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				// not yet authenticated (defaults to Http403ForbiddenEntryPoint)
 				.authenticationEntryPoint(new Http401UnauthorizedEntryPoint())
 				.and()
+			.csrf()
+				// Allow unsecured requests to Togglz and H2 consoles.
+				// See also: https://github.com/togglz/togglz/issues/119
+				// See also: src/main/config/test-override-web.xml
+				.requireCsrfProtectionMatcher(new AllExceptUrlsStartedWith("/togglz", "/console"))
+			.and()
 			.rememberMe()
 				// TODO: GH #27
 				.disable()
@@ -132,6 +140,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		provider.setUserDetailsService(getUserDetailsService());
 		provider.setMessageSource(messageSource);
 		return provider;
+	}
+	
+	private static class AllExceptUrlsStartedWith implements RequestMatcher {
+		
+		private static final String[] ALLOWED_METHODS =
+			new String[] {"GET", "HEAD", "TRACE", "OPTIONS"};
+		
+		private final String[] allowedUrls;
+		
+		public AllExceptUrlsStartedWith(String... allowedUrls) {
+			this.allowedUrls = allowedUrls;
+		}
+		
+		@Override
+		public boolean matches(HttpServletRequest request) {
+			// replicate default behavior (see CsrfFilter.DefaultRequiresCsrfMatcher class)
+			String method = request.getMethod();
+			for (String allowedMethod : ALLOWED_METHODS) {
+				if (allowedMethod.equals(method)) {
+					return false;
+				}
+			}
+			
+			// apply our own exceptions
+			String uri = request.getRequestURI();
+			for (String allowedUrl : allowedUrls) {
+				if (uri.startsWith(allowedUrl)) {
+					return false;
+				}
+			}
+			
+			return true;
+		}
+		
 	}
 	
 }

src/main/config/test-override-web.xml
@@ -26,6 +26,7 @@
 	
 	<servlet-mapping>
 		<servlet-name>H2Console</servlet-name>
+		<!-- See also src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java -->
 		<url-pattern>/console/*</url-pattern>
 	</servlet-mapping>
 	