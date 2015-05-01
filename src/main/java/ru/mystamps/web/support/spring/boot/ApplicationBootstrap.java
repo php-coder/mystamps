76bafad4510152936f80770cacde4ce8de656d79
Slava Semushin <slava.semushin@gmail.com>
2015-05-01 21:32:55 +0300

    Use our ApplicationContext.
    Fix the following exception during startup:
    java.lang.IllegalStateException: Cannot find changelog location: class path resource [db/changelog/db.changelog-master.yaml] (please add changelog or check your Liquibase configuration)

--- src/main/java/ru/mystamps/web/support/spring/boot/ApplicationBootstrap.java
+++ src/main/java/ru/mystamps/web/support/spring/boot/ApplicationBootstrap.java
@@ -2,8 +2,12 @@ package ru.mystamps.web.support.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import ru.mystamps.web.config.ApplicationContext;
 
 @EnableAutoConfiguration
+@Import(ApplicationContext.class)
 public class ApplicationBootstrap {
 	
 	public static void main(String[] args) {
