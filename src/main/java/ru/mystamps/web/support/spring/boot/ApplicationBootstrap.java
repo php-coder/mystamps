2b05d6848ea862e4b15a200a10403679a034b323
Slava Semushin <slava.semushin@gmail.com>
2015-05-03 14:40:11 +0300

    Fix showing of whitelabel error page instead of index page.
    Fix the following error when accessing index page:
    
    This application has no explicit mapping for /error, so you are seeing this as a fallback.
    There was an unexpected error (type=Not Found, status=404).

--- src/main/java/ru/mystamps/web/support/spring/boot/ApplicationBootstrap.java
+++ src/main/java/ru/mystamps/web/support/spring/boot/ApplicationBootstrap.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 import ru.mystamps.web.config.ApplicationContext;
+import ru.mystamps.web.config.DispatcherServletContext;
 
 @EnableAutoConfiguration
-@Import(ApplicationContext.class)
+@Import({
+	ApplicationContext.class,
+	DispatcherServletContext.class
+})
 public class ApplicationBootstrap {
 	
 	public static void main(String[] args) {
