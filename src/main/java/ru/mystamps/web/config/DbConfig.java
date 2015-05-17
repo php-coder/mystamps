395f38e18064b984e5e2afe3969073684a4e4348
Slava Semushin <slava.semushin@gmail.com>
2015-05-17 18:27:17 +0300

    Use autoconfiguration for datasources.

--- src/main/java/ru/mystamps/web/config/DbConfig.java
+++ src/main/java/ru/mystamps/web/config/DbConfig.java
@@ -25,7 +25,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -42,7 +41,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableTransactionManagement
 @EnableJpaRepositories("ru.mystamps.web.dao")
 @PropertySource("classpath:${spring.profiles.active}/spring/database.properties")
-@Import(DataSourceConfig.class)
 public class DbConfig {
 	
 	@Inject
