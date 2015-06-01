1442ea09233f688d0b3fe7af5bb3e49d9f108240
Slava Semushin <slava.semushin@gmail.com>
2015-06-01 13:58:47 +0400

    SecurityConfig: replace deprecated @EnableWebMvcSecurity by @EnableWebSecurity
    @see https://jira.spring.io/browse/SEC-2790
    
    No functional changes.

--- src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java
+++ src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java
@@ -36,15 +36,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import ru.mystamps.web.config.ServicesConfig;
 import ru.mystamps.web.Url;
 
-@EnableWebMvcSecurity
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
