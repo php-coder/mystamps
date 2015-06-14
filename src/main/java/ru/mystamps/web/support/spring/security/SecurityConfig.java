ae25712241c783f2108e4190ea0306e75180e267
Slava Semushin <slava.semushin@gmail.com>
2015-06-14 14:03:06 +0300

    SecurityConfig: add @Override annotation.
    Correction for a33e0043e5a8de2986f14e29b7b03b1c2695c660 commit.
    
    No functional changes.

--- src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java
+++ src/main/java/ru/mystamps/web/support/spring/security/SecurityConfig.java
@@ -107,6 +107,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Inject
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) {
 		auth.authenticationProvider(getAuthenticationProvider());
 	}
