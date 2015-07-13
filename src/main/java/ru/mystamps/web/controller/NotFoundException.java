beb6417dd080d589ba9fe2fe160b2ecfd4c7bebd
Slava Semushin <slava.semushin@gmail.com>
2015-07-13 13:56:57 +0400

    pom.xml: update Spring Framework to 4.2.0.RC2
    @see http://spring.io/blog/2015/06/30/spring-framework-4-2-rc2-available-now
    
    Also I've fixed empty page displaying instead of error page by setting the
    reason of error. I'm not sure is that a fix or temporary workaround. Probably,
    it will be fixed in 4.2.RC3 version.
    
    @see https://jira.spring.io/browse/SPR-13226
    
    No functional changes.

--- src/main/java/ru/mystamps/web/controller/NotFoundException.java
+++ src/main/java/ru/mystamps/web/controller/NotFoundException.java
@@ -20,7 +20,7 @@ package ru.mystamps.web.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not Found")
 public class NotFoundException extends RuntimeException {
 }
 
