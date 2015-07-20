9255a5b34fdeb41b806ffc7290066fb42dcf0604
Slava Semushin <slava.semushin@gmail.com>
2015-07-20 14:51:34 +0400

    /series/{id}: fix determination of presence series in collection.
    Correction for 27d12040eae17214378a8594b59dfb397723e2a2 commit.
    
    After feedback from: Pavel Finkelshtein <pavel.finkelshtein@gmail.com>

--- src/main/java/ru/mystamps/web/entity/Series.java
+++ src/main/java/ru/mystamps/web/entity/Series.java
@@ -47,7 +47,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString(exclude = "metaInfo")
-@EqualsAndHashCode(exclude = "metaInfo")
+@EqualsAndHashCode(exclude = { "metaInfo", "images" })
 @SuppressWarnings("PMD.TooManyFields")
 public class Series {
 	
