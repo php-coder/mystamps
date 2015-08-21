951c65e00a0174ec020ef412e8cd2509d62c541f
Slava Semushin <slava.semushin@gmail.com>
2015-08-21 22:14:11 +0300

    pom.xml: update Spring Framework to 4.2.0.RELEASE
    Also I removed workaround for https://jira.spring.io/browse/SPR-13226 and use Spring version from
    parent pom.
    
    @see https://spring.io/blog/2015/07/15/spring-framework-4-2-rc3-released-ga-on-july-30
    @see https://spring.io/blog/2015/07/31/spring-framework-4-2-goes-ga
    
    Fix #104
    
    No functional changes.

--- src/main/java/ru/mystamps/web/controller/NotFoundException.java
+++ src/main/java/ru/mystamps/web/controller/NotFoundException.java
@@ -20,7 +20,7 @@ package ru.mystamps.web.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not Found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 }
 
