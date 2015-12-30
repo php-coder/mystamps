2e269f1ccc420d650f19cb3b1cf319bb67309ff3
Slava Semushin <slava.semushin@gmail.com>
2015-12-30 20:32:35 +0100

    ValidationRules: inline constant.
    Removed last reference to Category entity.
    
    Addressed to #120
    
    No functional changes.

--- src/main/java/ru/mystamps/web/validation/ValidationRules.java
+++ src/main/java/ru/mystamps/web/validation/ValidationRules.java
@@ -17,7 +17,6 @@
  */
 package ru.mystamps.web.validation;
 
-import ru.mystamps.web.entity.Category;
 import ru.mystamps.web.entity.Country;
 import ru.mystamps.web.entity.Series;
 import ru.mystamps.web.entity.User;
@@ -41,7 +40,7 @@ public final class ValidationRules {
 	public static final String ACT_KEY_REGEXP = "[0-9a-z]+";
 	
 	public static final int CATEGORY_NAME_MIN_LENGTH = 3;
-	public static final int CATEGORY_NAME_MAX_LENGTH = Category.NAME_LENGTH;
+	public static final int CATEGORY_NAME_MAX_LENGTH = 50;
 	public static final String CATEGORY_NAME_EN_REGEXP = "[- a-zA-Z]+";
 	public static final String CATEGORY_NAME_RU_REGEXP = "[- а-яёА-ЯЁ]+";
 	public static final String CATEGORY_NAME_NO_HYPHEN_REGEXP = "[ \\p{L}]([- \\p{L}]+[ \\p{L}])*";
