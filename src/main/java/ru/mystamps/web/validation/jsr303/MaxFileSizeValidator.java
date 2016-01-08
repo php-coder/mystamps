c58dc519d4df03bcc9867b563bbd314a4db34048
Sergey Chechenev <cssru@mail.ru>
2016-01-08 14:09:36 +0300

    Valdiate that uploaded files not greater than 500 Kb.
    Fix #129

---
+++ src/main/java/ru/mystamps/web/validation/jsr303/MaxFileSizeValidator.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2009-2016 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package ru.mystamps.web.validation.jsr303;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Sergey Chechenev
+ */
+public class MaxFileSizeValidator implements ConstraintValidator<MaxFileSize, MultipartFile> {
+	private long maxFileSizeInBytes;
+	
+	@Override
+	public void initialize(MaxFileSize annotation) {
+		maxFileSizeInBytes = annotation.value() * annotation.unit().getSize();
+	}
+	
+	@Override
+	public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
+		
+		if (file == null) {
+			return true;
+		}
+		
+		return file.getSize() <= maxFileSizeInBytes;
+	}
+}
