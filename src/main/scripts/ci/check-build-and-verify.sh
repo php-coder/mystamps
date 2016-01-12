651a85efb35460ac0d8aa38458f0bd5549576ff5
Slava Semushin <slava.semushin@gmail.com>
2016-01-12 22:26:52 +0100

    .travis.yml: for travis profile run only integration tests.
    No functional changes.

--- src/main/scripts/ci/check-build-and-verify.sh
+++ src/main/scripts/ci/check-build-and-verify.sh
@@ -1,33 +1,47 @@
 #!/bin/sh
 
+RUN_ONLY_INTEGRATION_TESTS=no
+if [ "$1" = '--only-integration-tests' ]; then
+	RUN_ONLY_INTEGRATION_TESTS=yes
+fi
+
 . "$(dirname "$0")/common.sh"
 
-mvn --batch-mode checkstyle:check -Dcheckstyle.violationSeverity=warning >cs.log 2>&1 || touch CS_FAIL
-mvn --batch-mode pmd:check >pmd.log 2>&1 || touch PMD_FAIL
-mvn --batch-mode license:check >license.log 2>&1 || touch LICENSE_FAIL
-find src -type f -name '*.html' | xargs bootlint >bootlint.log 2>&1 || touch BOOTLINT_FAIL
+if [ "$RUN_ONLY_INTEGRATION_TESTS" = 'no' ]; then
+	mvn --batch-mode checkstyle:check -Dcheckstyle.violationSeverity=warning >cs.log 2>&1 || touch CS_FAIL
+	mvn --batch-mode pmd:check >pmd.log 2>&1 || touch PMD_FAIL
+	mvn --batch-mode license:check >license.log 2>&1 || touch LICENSE_FAIL
+	find src -type f -name '*.html' | xargs bootlint >bootlint.log 2>&1 || touch BOOTLINT_FAIL
+	mvn --batch-mode jasmine:test >jasmine.log 2>&1 || touch JASMINE_FAIL
+fi
+
 mvn --batch-mode verify >verify.log 2>&1 || touch VERIFY_FAIL
-mvn --batch-mode jasmine:test >jasmine.log 2>&1 || touch JASMINE_FAIL
 
 echo
 echo 'Build summary:'
 echo
 
-print_status CS_FAIL       'Run CheckStyle'
-print_status PMD_FAIL      'Run PMD'
-print_status LICENSE_FAIL  'Check license headers'
-print_status BOOTLINT_FAIL 'Run bootlint'
+if [ "$RUN_ONLY_INTEGRATION_TESTS" = 'no' ]; then
+	print_status CS_FAIL       'Run CheckStyle'
+	print_status PMD_FAIL      'Run PMD'
+	print_status LICENSE_FAIL  'Check license headers'
+	print_status BOOTLINT_FAIL 'Run bootlint'
+	print_status JASMINE_FAIL  'Run JavaScript unit tests'
+fi
+
 print_status VERIFY_FAIL   'Compile and run unit/integration tests'
-print_status JASMINE_FAIL  'Run JavaScript unit tests'
 
 echo
 
-print_log cs.log       'Run CheckStyle'
-print_log pmd.log      'Run PMD'
-print_log license.log  'Check license headers'
-print_log bootlint.log 'Run bootlint'
+if [ "$RUN_ONLY_INTEGRATION_TESTS" = 'no' ]; then
+	print_log cs.log       'Run CheckStyle'
+	print_log pmd.log      'Run PMD'
+	print_log license.log  'Check license headers'
+	print_log bootlint.log 'Run bootlint'
+	print_log jasmine.log  'Run JavaScript unit tests'
+fi
+
 print_log verify.log   'Compile and run unit/integration tests'
-print_log jasmine.log  'Run JavaScript unit tests'
 
 rm -f cs.log pmd.log license.log bootlint.log verify.log jasmine.log
 
