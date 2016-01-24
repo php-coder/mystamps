6897d415e2bf939db71f6c330f7d1a59dc688608
Slava Semushin <slava.semushin@gmail.com>
2016-01-24 21:03:08 +0100

    ValidationRules: inline constant.
    Removed last reference to Series entity.
    
    Addressed to #120
    
    No functional changes.

--- src/main/java/ru/mystamps/web/validation/ValidationRules.java
+++ src/main/java/ru/mystamps/web/validation/ValidationRules.java
@@ -17,7 +17,6 @@
  */
 package ru.mystamps.web.validation;
 
-import ru.mystamps.web.entity.Series;
 import ru.mystamps.web.entity.User;
 import ru.mystamps.web.entity.UsersActivation;
 
@@ -53,7 +52,7 @@ public final class ValidationRules {
 	public static final int MIN_STAMPS_IN_SERIES = 1;
 	public static final int MAX_STAMPS_IN_SERIES = 50;
 	public static final int MIN_RELEASE_YEAR     = 1840;
-	public static final int MAX_SERIES_COMMENT_LENGTH = Series.COMMENT_LENGTH;
+	public static final int MAX_SERIES_COMMENT_LENGTH = 255;
 	public static final String CATALOG_NUMBERS_REGEXP = "[1-9][0-9]{0,3}(,[1-9][0-9]{0,3})*";
 	
 	/** Maximum uploading image size in kilobytes. */
