8c7d1818d656ec7008749ef8fe940daed3a193d3
Slava Semushin <slava.semushin@gmail.com>
2016-03-06 00:19:01 +0100

    Revert "src/main/scripts/ci: activate errexit, nounset and pipefail options."
    This change break the build and colorized output.
    
    This reverts commit 5c10fa5d4ef14b770b54f836e7c2a07633326eea.

--- src/main/scripts/ci/common.sh
+++ src/main/scripts/ci/common.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-# Treat unset variables and parameters as an error when performing parameter expansion
-set -o nounset
-
-# Exit immediately if command returns a non-zero status
-set -e errexit
-
-# Return value of a pipeline is the value of the last command to exit with a non-zero status
-set -o pipefail
-
 print_status() {
 	local FAIL_FILE="$1"
 	local MSG="$2"
--- src/main/scripts/ci/publish-code-coverage.sh
+++ src/main/scripts/ci/publish-code-coverage.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-
-# Treat unset variables and parameters as an error when performing parameter expansion
-set -o nounset
-
-# Exit immediately if command returns a non-zero status
-set -e errexit
-
-# Return value of a pipeline is the value of the last command to exit with a non-zero status
-set -o pipefail
-
+#!/bin/sh
 
 . "$(dirname "$0")/common.sh"
 
