16e08959387ff9576ffaccfdca21364f3e914d54
Slava Semushin <slava.semushin@gmail.com>
2016-03-05 23:14:04 +0100

    src/main/scripts/ci/common.sh: use local keyword.
    No functional changes.

--- src/main/scripts/ci/common.sh
+++ src/main/scripts/ci/common.sh
@@ -1,6 +1,6 @@
 print_status() {
-	FAIL_FILE="$1"
-	MSG="$2"
+	local FAIL_FILE="$1"
+	local MSG="$2"
 	
 	printf "* $MSG... "
 	if [ -f "$FAIL_FILE" ]; then
@@ -11,8 +11,8 @@ print_status() {
 }
 
 print_log() {
-	LOG_FILE="$1"
-	MSG="$2"
+	local LOG_FILE="$1"
+	local MSG="$2"
 	
 	echo
 	echo "=====> \033[1;33m$MSG\033[0m"
