6792b6fd833e854342c7684d213607c0fa2c891a
Slava Semushin <slava.semushin@gmail.com>
2016-04-20 19:12:24 +0200

    publish-code-coverage.sh: don't run enforcer and minify during publishing code coverage.
    No functional changes.

--- src/main/scripts/ci/publish-code-coverage.sh
+++ src/main/scripts/ci/publish-code-coverage.sh
@@ -4,7 +4,7 @@
 
 JACOCO_FAIL=
 
-mvn --batch-mode jacoco:prepare-agent test jacoco:report coveralls:jacoco >jacoco.log 2>&1 || JACOCO_FAIL=yes
+mvn --batch-mode jacoco:prepare-agent test jacoco:report coveralls:jacoco -Denforcer.skip=true -DskipMinify=true >jacoco.log 2>&1 || JACOCO_FAIL=yes
 
 print_status "$JACOCO_FAIL" 'Publish code coverage'
 
