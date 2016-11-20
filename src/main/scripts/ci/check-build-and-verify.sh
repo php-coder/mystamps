699e1478b5c563c8076ee146bbbca3d9b4d0902c
Slava Semushin <slava.semushin@gmail.com>
2016-11-20 13:33:03 +0100

    Ignore warnings from htmlvalidator about empty option.
    WARNING:html5validator.validator:"file:src/main/webapp/WEB-INF/views/series/info.html":428.16-428.35:
    error: The first child "option" element of a "select" element with a "required" attribute, and
    without a "multiple" attribute, and without a "size" attribute whose value is greater than "1", must
    have either an empty "value" attribute, or must have no text content. Consider either adding a
    placeholder option label, or adding a "size" attribute with a value equal to the number of "option"
    elements.
    
    When I'm adding empty option then there is no way to set a default value. In Safari and Firefox
    "selected" attribute doesn't work.
    
    Should be in fe9f30daefe5ce0f76cd817c433062600676c404 commit.
    
    No functional changes.

--- src/main/scripts/ci/check-build-and-verify.sh
+++ src/main/scripts/ci/check-build-and-verify.sh
@@ -39,6 +39,7 @@ if [ "$RUN_ONLY_INTEGRATION_TESTS" = 'no' ]; then
 			'Attribute “(th|sec|togglz):[a-z]+” is not serializable' \
 			'Attribute with the local name “xmlns:[a-z]+” is not serializable' \
 			'An "img" element must have an "alt" attribute' \
+			'The first child "option" element of a "select" element with a "required" attribute' \
 		--show-warnings \
 		>validator.log 2>&1 || HTML_FAIL=yes
 	mvn --batch-mode test \
