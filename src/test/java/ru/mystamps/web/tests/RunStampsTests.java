684e3d66ad31692b7b59f2e4091cc0253d059633
Goblic <mallfromail@mail.ru>
2016-12-07 23:22:20 +0200

    clean empty field

--- src/test/java/ru/mystamps/web/tests/RunStampsTests.java
+++ src/test/java/ru/mystamps/web/tests/RunStampsTests.java
@@ -1,19 +1,14 @@
 package ru.mystamps.web.tests;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
-/**
- * Created by Goblik on 30.10.2016.
- */
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features",
 	plugin = {"pretty",
 		"html:target/cucumber-html-report"},
-	tags = {"@in1"})
+	tags = {"@in12"})
 
 public class RunStampsTests {
 
