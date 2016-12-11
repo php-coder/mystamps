f3f7a5b472aa6e15f1d6afbf4b47a44c8290ecc7
Goblic <mallfromail@mail.ru>
2016-12-11 22:26:36 +0300

    if the button "Remove" present on the page

--- src/test/java/ru/mystamps/web/tests/StepDefinitions.java
+++ src/test/java/ru/mystamps/web/tests/StepDefinitions.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class StepDefinitions {
@@ -28,10 +29,13 @@ public class StepDefinitions {
 	public void iAddSeriesToMyCollection() throws Exception {
 		driver.get("http://127.0.0.1:8080/");
 		driver.findElement(By.linkText("1 item(s)")).click();
-		driver.findElement(By.cssSelector("input.btn.btn-success")).click();
-		
+		if (driver.findElements(By.cssSelector("input.btn.btn-danger")).size() != 0) {
+			System.out.println("Series is part of your collection");
+		}else {
+			driver.findElement(By.cssSelector("input.btn.btn-success")).click();
+		}
 	}
-
+	
 	@Then("^I am on the page with my collection$")
 	public void i_am_on_the_page_with_my_collection() throws Exception {
 		driver.get("http://127.0.0.1:8080/collection/coder");
