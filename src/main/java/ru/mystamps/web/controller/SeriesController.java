1c9a5f7222f0ba35f54c2ade16ff0dfeaac12f20
Slava Semushin <slava.semushin@gmail.com>
2017-01-09 12:26:48 +0100

    SeriesController: remove unused import.
    Fixes PMD/CheckStyle warnings and unbreak building.
    
    Should be in e0e22c50b583219d97eb460efdbf3abbae3a962a commit.

--- src/main/java/ru/mystamps/web/controller/SeriesController.java
+++ src/main/java/ru/mystamps/web/controller/SeriesController.java
@@ -43,7 +43,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
