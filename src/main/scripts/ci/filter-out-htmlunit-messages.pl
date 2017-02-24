4683e843bf2eab9a36221333b71cec651ce3fe62
Slava Semushin <slava.semushin@gmail.com>
2017-02-24 22:10:15 +0100

    check-build-and-verify.sh: rewrite sed script on Perl.
    It turned out that `sed` from Travis CI doesn't support -z option.
    
    Should be in feea131a36f0cbae5c31a3826c4d061c878fe114 commit.
    
    Addressed to #538

---
+++ src/main/scripts/ci/filter-out-htmlunit-messages.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+#
+# This script is filtering out log messages from htmlunit.
+# It's a workaround for https://github.com/php-coder/mystamps/issues/538
+#
+
+use strict;
+use warnings;
+
+my @regexps = (
+	# [WARNING] CSS error: 'http://127.0.0.1:8081/public/bootstrap/3.3.7/css/bootstrap.min.css' [5:56298] Error in expression; ':' found after identifier "progid".
+	qr/\[WARNING\] CSS error: '[^']+' \[[^]]+\] Error in expression; [^\.]+\.\n/,
+	
+	# [WARNING] CSS error: 'http://127.0.0.1:8081/public/bootstrap/3.3.7/css/bootstrap.min.css' [5:115558] Invalid color "#000\9".
+	qr/\[WARNING\] CSS error: '[^']+' \[[^]]+\] Invalid color "[^"]+"\.\n/,
+	
+	# [WARNING] CSS error: 'http://127.0.0.1:8081/public/selectize/0.12.3/css/selectize.bootstrap3.css' [176:3] Error in declaration. '*' is not allowed as first char of a property.
+	qr/\[WARNING\] CSS error: '[^']+' \[[^]]+\] Error in declaration\. '\*' is not allowed as first char of a property\.\n/,
+	
+	# [ERROR] runtimeError: message=[An invalid or illegal selector was specified (selector: '*,:x' error: Invalid selector: :x).] sourceName=[http://127.0.0.1:8081/public/jquery/1.9.1/jquery.min.js] line=[4] lineSource=[null] lineOffset=[0]
+	qr/\[ERROR\] runtimeError: message=\[[^]]+\] sourceName=\[[^]]+\] line=\[[^]]+\] lineSource=\[[^]]+\] lineOffset=\[[^]]+\]\n/,
+
+	# [ERROR] runtimeError: message=[An invalid or illegal selector was specified (selector: '[id='sizzle-1487943406281'] [data-selectable]:first' error: Invalid selector: [id="sizzle-1487943406281"] [data-selectable]:first).] sourceName=[http://127.0.0.1:8081/public/jquery/1.9.1/jquery.min.js] line=[4] lineSource=[null] lineOffset=[0]
+	qr/\[ERROR\] runtimeError: message=\[[^\(]+\(selector: '\[id='sizzle-[^']+'\] \[data-selectable\]:first' error: Invalid selector: \[id="sizzle-[^"]+"\] \[data-selectable\]:first\)\.\] sourceName=\[[^]]+\] line=\[[^]]+\] lineSource=\[[^]]+\] lineOffset=\[[^]]+\]\n/,
+	
+	# [INFO] Bad input type: "url", creating a text input
+	qr/\[INFO\] Bad input type: "url", creating a text input\n/
+);
+
+foreach my $line (<STDIN>) {
+	foreach my $regexp (@regexps) {
+		my $count = $line =~ s/$regexp//;
+		if ($count > 0) {
+			# replacement were performed, skip other regexps
+			last;
+		}
+	}
+	
+	print $line;
+}
