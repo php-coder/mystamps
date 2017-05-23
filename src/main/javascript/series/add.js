1048e4dd6094b07ec45fac28b420517e9ae52ea2
Slava Semushin <slava.semushin@gmail.com>
2017-05-23 10:49:11 +0200

    series/add.js(initPage): add name to a function.
    No functional changes.

--- src/main/javascript/series/add.js
+++ src/main/javascript/series/add.js
@@ -5,7 +5,7 @@
 function initPage(suggestCountryUrl) {
 	$('#country').selectize();
 	
-	$('.js-catalog-numbers').on('blur', function() {
+	$('.js-catalog-numbers').on('blur', function expandCatalogNumbers() {
 		$(this).val(function(idx, val) {
 			return CatalogUtils.expandNumbers(val);
 		});
