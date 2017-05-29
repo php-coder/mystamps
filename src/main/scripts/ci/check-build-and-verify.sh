7da91a53a1d7f43670d67adfce359814fa7ee27a
Slava Semushin <slava.semushin@gmail.com>
2017-05-29 16:31:46 +0200

    check-build-and-verify.sh: skip some checks when related files weren't modified.
    Now the checks will be run only when the files with the following patterns were modified:
    
    - sortpom:    pom.xml
    - enforcer:   pom.xml
    - checkstyle: pom.xml, *.java, *.properties, checkstyle.xml, checkstyle-suppressions.xml
    - findbugs:   pom.xml, *.java, findbugs-filter.xml
    - pmd:        pom.xml, *.java, pmd.xml
    - unit tests: pom.xml, *.java, *.groovy
    - license:    pom.xml, *.java, *.groovy, license_header.txt
    - codenarc:   pom.xml, *.groovy
    - jasmine:    pom.xml, *.js
    - rflint:    .travis.yml, *.robot
    - bootlint:  .travis.yml *.html
    - html5validator: .travis.yml *.html
    
    Integration tests are executed always.
    
    Fix #423

--- src/main/scripts/ci/check-build-and-verify.sh
+++ src/main/scripts/ci/check-build-and-verify.sh
@@ -34,53 +34,140 @@ DANGER_STATUS=
 
 if [ "$RUN_ONLY_INTEGRATION_TESTS" = 'no' ]; then
 	
-	mvn --batch-mode checkstyle:check -Dcheckstyle.violationSeverity=warning \
-		>cs.log 2>&1 || CS_STATUS=fail
+	# TRAVIS_COMMIT_RANGE: The range of commits that were included in the push or
+	# pull request. (Note that this is empty for builds triggered by the initial
+	# commit of a new branch.)
+	if [ -n "${TRAVIS_COMMIT_RANGE:-}" ]; then
+		echo
+		echo "INFO: Range of the commits to be checked: $TRAVIS_COMMIT_RANGE"
+		echo 'INFO: List of the files modified by this commits range:'
+		git --no-pager diff --name-only $TRAVIS_COMMIT_RANGE -- | sed 's|^|      |' || :
+		
+		MODIFIED_FILES="$(git --no-pager diff --name-only $TRAVIS_COMMIT_RANGE -- 2>/dev/null || :)"
+		
+		if [ -n "$MODIFIED_FILES" ]; then
+			AFFECTS_POM_XML="$(echo "$MODIFIED_FILES"      | fgrep -xq 'pom.xml' || echo 'no')"
+			AFFECTS_TRAVIS_CFG="$(echo "$MODIFIED_FILES"   | fgrep -xq '.travis.yml' || echo 'no')"
+			AFFECTS_CS_CFG="$(echo "$MODIFIED_FILES"        | egrep -q '(checkstyle\.xml|checkstyle-suppressions\.xml)$' || echo 'no')"
+			AFFECTS_FB_CFG="$(echo "$MODIFIED_FILES"        |  grep -q 'findbugs-filter\.xml$' || echo 'no')"
+			AFFECTS_PMD_XML="$(echo "$MODIFIED_FILES"       |  grep -q 'pmd\.xml$' || echo 'no')"
+			AFFECTS_JS_FILES="$(echo "$MODIFIED_FILES"      |  grep -q '\.js$' || echo 'no')"
+			AFFECTS_HTML_FILES="$(echo "$MODIFIED_FILES"    |  grep -q '\.html$' || echo 'no')"
+			AFFECTS_JAVA_FILES="$(echo "$MODIFIED_FILES"    |  grep -q '\.java$' || echo 'no')"
+			AFFECTS_ROBOT_FILES="$(echo "$MODIFIED_FILES"   |  grep -q '\.robot$' || echo 'no')"
+			AFFECTS_GROOVY_FILES="$(echo "$MODIFIED_FILES"  |  grep -q '\.groovy$' || echo 'no')"
+			AFFECTS_PROPERTIES="$(echo "$MODIFIED_FILES"    |  grep -q '\.properties$' || echo 'no')"
+			AFFECTS_LICENSE_HEADER="$(echo "$MODIFIED_FILES" | grep -q 'license_header\.txt$' || echo 'no')"
+			
+			if [ "$AFFECTS_POM_XML" = 'no' ]; then
+				POM_STATUS=skip
+				ENFORCER_STATUS=skip
+				
+				if [ "$AFFECTS_JAVA_FILES" = 'no' ]; then
+					[ "$AFFECTS_FB_CFG" != 'no' ] || FINDBUGS_STATUS=skip
+					[ "$AFFECTS_CS_CFG" != 'no' -o "$AFFECTS_PROPERTIES" != 'no' ] || CS_STATUS=skip
+					[ "$AFFECTS_PMD_XML" != 'no' ] || PMD_STATUS=skip
+					
+					if [ "$AFFECTS_GROOVY_FILES" = 'no' ]; then
+						TEST_STATUS=skip
+						
+						[ "$AFFECTS_LICENSE_HEADER" != 'no' ] || LICENSE_STATUS=skip
+					fi
+				fi
+				
+				[ "$AFFECTS_GROOVY_FILES" != 'no' ] || CODENARC_STATUS=skip
+				[ "$AFFECTS_JS_FILES" != 'no' ] || JASMINE_STATUS=skip
+			fi
+			
+			if [ "$AFFECTS_TRAVIS_CFG" = 'no' ]; then
+				if [ "$AFFECTS_HTML_FILES" = 'no' ]; then
+					BOOTLINT_STATUS=skip
+					HTML_STATUS=skip
+				fi
+				[ "$AFFECTS_ROBOT_FILES" != 'no' ] || RFLINT_STATUS=skip
+			fi
+			echo 'INFO: Some checks could be skipped'
+		else
+			echo "INFO: Couldn't determine list of modified files."
+			echo 'INFO: All checks will be performed'
+		fi
+	else
+		echo
+		echo "INFO: Couldn't determine a range of the commits: \$TRAVIS_COMMIT_RANGE is empty."
+		echo 'INFO: All checks will be performed'
+	fi
 	
-	mvn --batch-mode pmd:check \
-		>pmd.log 2>&1 || PMD_STATUS=fail
+	if [ "$CS_STATUS" != 'skip' ]; then
+		mvn --batch-mode checkstyle:check -Dcheckstyle.violationSeverity=warning \
+			>cs.log 2>&1 || CS_STATUS=fail
+	fi
 	
-	mvn --batch-mode codenarc:codenarc -Dcodenarc.maxPriority1Violations=0 -Dcodenarc.maxPriority2Violations=0 -Dcodenarc.maxPriority3Violations=0 \
-		>codenarc.log 2>&1 || CODENARC_STATUS=fail
+	if [ "$PMD_STATUS" != 'skip' ]; then
+		mvn --batch-mode pmd:check \
+			>pmd.log 2>&1 || PMD_STATUS=fail
+	fi
 	
-	mvn --batch-mode license:check \
-		>license.log 2>&1 || LICENSE_STATUS=fail
+	if [ "$CODENARC_STATUS" != 'skip' ]; then
+		mvn --batch-mode codenarc:codenarc -Dcodenarc.maxPriority1Violations=0 -Dcodenarc.maxPriority2Violations=0 -Dcodenarc.maxPriority3Violations=0 \
+			>codenarc.log 2>&1 || CODENARC_STATUS=fail
+	fi
 	
-	mvn --batch-mode sortpom:verify -Dsort.verifyFail=stop \
-		>pom.log || POM_STATUS=fail
+	if [ "$LICENSE_STATUS" != 'skip' ]; then
+		mvn --batch-mode license:check \
+			>license.log 2>&1 || LICENSE_STATUS=fail
+	fi
 	
-	find src -type f -name '*.html' | xargs bootlint \
-		>bootlint.log 2>&1 || BOOTLINT_STATUS=fail
+	if [ "$POM_STATUS" != 'skip' ]; then
+		mvn --batch-mode sortpom:verify -Dsort.verifyFail=stop \
+			>pom.log || POM_STATUS=fail
+	fi
 	
-	rflint --error=all --ignore TooFewKeywordSteps --ignore TooManyTestSteps --configure LineTooLong:130 src/test/robotframework \
-		>rflint.log 2>&1 || RFLINT_STATUS=fail
+	if [ "$BOOTLINT_STATUS" != 'skip' ]; then
+		find src -type f -name '*.html' | xargs bootlint \
+			>bootlint.log 2>&1 || BOOTLINT_STATUS=fail
+	fi
 	
-	mvn --batch-mode jasmine:test \
-		>jasmine.log 2>&1 || JASMINE_STATUS=fail
+	if [ "$RFLINT_STATUS" != 'skip' ]; then
+		rflint --error=all --ignore TooFewKeywordSteps --ignore TooManyTestSteps --configure LineTooLong:130 src/test/robotframework \
+			>rflint.log 2>&1 || RFLINT_STATUS=fail
+	fi
 	
-	# FIXME: add check for src/main/config/nginx/503.*html
-	# TODO: remove ignoring of error about alt attribute after resolving #314
-	# TODO: remove ignoring of error about document language when it will be resolved in upstream
-	html5validator \
-		--root src/main/webapp/WEB-INF/views \
-		--ignore-re 'Attribute “(th|sec|togglz|xmlns):[a-z]+” not allowed' \
-			'Attribute “(th|sec|togglz):[a-z]+” is not serializable' \
-			'Attribute with the local name “xmlns:[a-z]+” is not serializable' \
-			'An "img" element must have an "alt" attribute' \
-			'The first child "option" element of a "select" element with a "required" attribute' \
-			'This document appears to be written in (Danish|Lithuanian)' \
-		--show-warnings \
-		>validator.log 2>&1 || HTML_STATUS=fail
+	if [ "$JASMINE_STATUS" != 'skip' ]; then
+		mvn --batch-mode jasmine:test \
+			>jasmine.log 2>&1 || JASMINE_STATUS=fail
+	fi
 	
-	mvn --batch-mode enforcer:enforce \
-		>enforcer.log 2>&1 || ENFORCER_STATUS=fail
+	if [ "$HTML_STATUS" != 'skip' ]; then
+		# FIXME: add check for src/main/config/nginx/503.*html
+		# TODO: remove ignoring of error about alt attribute after resolving #314
+		# TODO: remove ignoring of error about document language when it will be resolved in upstream
+		html5validator \
+			--root src/main/webapp/WEB-INF/views \
+			--ignore-re 'Attribute “(th|sec|togglz|xmlns):[a-z]+” not allowed' \
+				'Attribute “(th|sec|togglz):[a-z]+” is not serializable' \
+				'Attribute with the local name “xmlns:[a-z]+” is not serializable' \
+				'An "img" element must have an "alt" attribute' \
+				'The first child "option" element of a "select" element with a "required" attribute' \
+				'This document appears to be written in (Danish|Lithuanian)' \
+			--show-warnings \
+			>validator.log 2>&1 || HTML_STATUS=fail
+	fi
 	
-	mvn --batch-mode test -Denforcer.skip=true -Dmaven.resources.skip=true -DskipMinify=true -DdisableXmlReport=false \
-		>test.log 2>&1 || TEST_STATUS=fail
+	if [ "$ENFORCER_STATUS" != 'skip' ]; then
+		mvn --batch-mode enforcer:enforce \
+			>enforcer.log 2>&1 || ENFORCER_STATUS=fail
+	fi
 	
-	# run after tests for getting compiled sources
-	mvn --batch-mode findbugs:check \
-		>findbugs.log 2>&1 || FINDBUGS_STATUS=fail
+	if [ "$TEST_STATUS" != 'skip' ]; then
+		mvn --batch-mode test -Denforcer.skip=true -Dmaven.resources.skip=true -DskipMinify=true -DdisableXmlReport=false \
+			>test.log 2>&1 || TEST_STATUS=fail
+	fi
+	
+	if [ "$FINDBUGS_STATUS" != 'skip' ]; then
+		# run after tests for getting compiled sources
+		mvn --batch-mode findbugs:check \
+			>findbugs.log 2>&1 || FINDBUGS_STATUS=fail
+	fi
 fi
 
 mvn --batch-mode verify -Denforcer.skip=true -DskipUnitTests=true \
@@ -121,18 +208,18 @@ fi
 echo
 
 if [ "$RUN_ONLY_INTEGRATION_TESTS" = 'no' ]; then
-	print_log cs.log        'Run CheckStyle'
-	print_log pmd.log       'Run PMD'
-	print_log codenarc.log  'Run CodeNarc'
-	print_log license.log   'Check license headers'
-	print_log pom.log       'Check sorting of pom.xml'
-	print_log bootlint.log  'Run bootlint'
-	print_log rflint.log    'Run robot framework lint'
-	print_log jasmine.log   'Run JavaScript unit tests'
-	print_log validator.log 'Run html5validator'
-	print_log enforcer.log  'Run maven-enforcer-plugin'
+	[ "$CS_STATUS" = 'skip' ]       || print_log cs.log        'Run CheckStyle'
+	[ "$PMD_STATUS" = 'skip' ]      || print_log pmd.log       'Run PMD'
+	[ "$CODENARC_STATUS" = 'skip' ] || print_log codenarc.log  'Run CodeNarc'
+	[ "$LICENSE_STATUS" = 'skip' ]  || print_log license.log   'Check license headers'
+	[ "$POM_STATUS" = 'skip' ]      || print_log pom.log       'Check sorting of pom.xml'
+	[ "$BOOTLINT_STATUS" = 'skip' ] || print_log bootlint.log  'Run bootlint'
+	[ "$RFLINT_STATUS" = 'skip' ]   || print_log rflint.log    'Run robot framework lint'
+	[ "$JASMINE_STATUS" = 'skip' ]  || print_log jasmine.log   'Run JavaScript unit tests'
+	[ "$HTML_STATUS" = 'skip' ]     || print_log validator.log 'Run html5validator'
+	[ "$ENFORCER_STATUS" = 'skip' ] || print_log enforcer.log  'Run maven-enforcer-plugin'
 	print_log test.log      'Run unit tests'
-	print_log findbugs.log  'Run FindBugs'
+	[ "$FINDBUGS_STATUS" = 'skip' ] || print_log findbugs.log  'Run FindBugs'
 fi
 
 print_log verify.log   'Run integration tests'
@@ -151,6 +238,6 @@ fi
 
 rm -f cs.log pmd.log codenarc.log license.log pom.log bootlint.log rflint.log jasmine.log validator.log enforcer.log test.log findbugs.log verify-raw.log verify.log danger.log
 
-if [ -n "$CS_STATUS$PMD_STATUS$CODENARC_STATUS$LICENSE_STATUS$POM_STATUS$BOOTLINT_STATUS$RFLINT_STATUS$JASMINE_STATUS$HTML_STATUS$ENFORCER_STATUS$TEST_STATUS$FINDBUGS_STATUS$VERIFY_STATUS$DANGER_STATUS" ]; then
+if echo "$CS_STATUS$PMD_STATUS$CODENARC_STATUS$LICENSE_STATUS$POM_STATUS$BOOTLINT_STATUS$RFLINT_STATUS$JASMINE_STATUS$HTML_STATUS$ENFORCER_STATUS$TEST_STATUS$FINDBUGS_STATUS$VERIFY_STATUS$DANGER_STATUS" | fgrep -qs 'fail'; then
 	exit 1
 fi
--- src/main/scripts/ci/common.sh
+++ src/main/scripts/ci/common.sh
@@ -1,6 +1,7 @@
 print_status() {
 	# $1 is empty if check has succeeded
 	# $1 equals to 'fail' if check has failed
+	# $1 equals to 'skip' if check has skipped
 	local result="$1"
 	local msg="$2"
 	
@@ -10,6 +11,9 @@ print_status() {
 	if [ "$result" = 'fail' ]; then
 		status='FAIL'
 		color=31
+	elif [ "$result" = 'skip' ]; then
+		status='SKIP'
+		color=33
 	fi
 	printf "* %s... \033[1;%dm%s\033[0m\n" "$msg" "$color" "$status"
 }
