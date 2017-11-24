8ce2a3c16c1a69e2ce6c60f7050c1476ab2458ee
Slava Semushin <slava.semushin@gmail.com>
2017-11-24 10:29:37 +0100

    SeriesImportController: fix import order.
    Correction for 5486a24cff6a2154c9f7ce95e51e669486a34b84 commit.
    
    No code changes.

--- src/main/java/ru/mystamps/web/controller/SeriesImportController.java
+++ src/main/java/ru/mystamps/web/controller/SeriesImportController.java
@@ -28,11 +28,11 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.WebDataBinder;
 
 import lombok.RequiredArgsConstructor;
 
