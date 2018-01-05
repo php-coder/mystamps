php-coder/mystamps

commit 7290c9a907d7eb419a1db228c87cf3c437701071

SeriesImportController.showRequestAndImportSeriesForm(): simplify code by removing extra if-conditions.
No functional changes.

src/main/java/ru/mystamps/web/controller/SeriesImportController.java
@@ -117,24 +117,12 @@ public class SeriesImportController {
 		
 		boolean hasParsedData = parsedData != null;
 		if (hasParsedData) {
-			if (parsedData.getCategory() != null) {
-				form.setCategory(parsedData.getCategory());
-			}
-			if (parsedData.getCountry() != null) {
-				form.setCountry(parsedData.getCountry());
-			}
-			if (parsedData.getImageUrl() != null) {
-				form.setImageUrl(parsedData.getImageUrl());
-			}
-			if (parsedData.getIssueYear() != null) {
-				form.setYear(parsedData.getIssueYear());
-			}
-			if (parsedData.getQuantity() != null) {
-				form.setQuantity(parsedData.getQuantity());
-			}
-			if (parsedData.getPerforated() != null) {
-				form.setPerforated(parsedData.getPerforated());
-			}
+			form.setCategory(parsedData.getCategory());
+			form.setCountry(parsedData.getCountry());
+			form.setImageUrl(parsedData.getImageUrl());
+			form.setYear(parsedData.getIssueYear());
+			form.setQuantity(parsedData.getQuantity());
+			form.setPerforated(parsedData.getPerforated());
 		}
 		
 		model.addAttribute("importSeriesForm", form);

commit 7a4cb5aca29bd26009958999a5a1b150378ee1d8

SeriesImportController.showRequestAndImportSeriesForm(): use default value for perforated flag when nothing were extracted.
Regression after 7290c9a907d7eb419a1db228c87cf3c437701071 commit.

src/main/java/ru/mystamps/web/controller/SeriesImportController.java
@@ -122,7 +122,9 @@ public class SeriesImportController {
 			form.setImageUrl(parsedData.getImageUrl());
 			form.setYear(parsedData.getIssueYear());
 			form.setQuantity(parsedData.getQuantity());
-			form.setPerforated(parsedData.getPerforated());
+			if (parsedData.getPerforated() != null) {
+				form.setPerforated(parsedData.getPerforated());
+			}
 		}
 		
 		model.addAttribute("importSeriesForm", form);