5709d0e2a08c75ccf2e82821d29936172d25f609
Slava Semushin <slava.semushin@gmail.com>
2018-06-22 22:11:37 +0200

    save result to a robot files.
    [ci skip]

--- sugest-rf-equivalent.py
+++ sugest-rf-equivalent.py
@@ -9,7 +9,8 @@ import re
 import sys
 import string
 
-basedir = 'src/test/java/ru/mystamps/web/tests/cases'
+basedir  = 'src/test/java/ru/mystamps/web/tests/cases'
+robotdir = 'src/test/robotframework'
 
 def load_property_file(filepath):
     props = {}
@@ -79,6 +80,11 @@ def handleTestMethod(lines):
                 suggested_list.append(res)
         if not found:
             res = handleGoTo(line)
+            if res != None:
+                found = True
+                suggested_list.append(res)
+        if not found:
+            res = handleLinkWithLabelExist(line)
             if res != None:
                 found = True
                 suggested_list.append(res)
@@ -163,24 +169,78 @@ def handleLocationShouldBe(line):
     value = value.replace('Url.ACTIVATE_ACCOUNT_PAGE', '${SITE_URL}/account/activate')
     return { 'name': 'Location Should Be', 'args': value }
 
+def handleLinkWithLabelExist(line):
+    res = re.search(r'assertThat\(page\.linkWithLabelExists\(tr\(\"([^"]+)', line)
+    if res == None:
+        return None
+    link = props[res.group(1)]
+    return { 'name': 'Page Should Contain Link', 'args': 'link=%s' % link }
+
+def write_robot_test(filename, test_method):
+    suggested_list = handleTestMethod(test_method)
+    if len(suggested_list) <= 1:
+        return False
+    if not filename in javaFiles2robot:
+        print('ERROR: could not find a corresponding robot file for %s' % filename, file=sys.stderr)
+        sys.exit(1)
+    robotFilename = javaFiles2robot[filename]
+    robotFile = os.path.join(robotdir, robotFilename)
+    print('Updating %s' % robotFile, file=sys.stderr)
+    inside_test_cases_block = False
+    append_our_case_now = False
+    for line in fileinput.FileInput(robotFile, inplace=True):
+        if line.startswith('*** Test Cases ***'):
+            inside_test_cases_block = True
+            pass
+        elif line.startswith('*** '):
+            if inside_test_cases_block:
+                inside_test_cases_block = False
+                append_our_case_now = True
+            pass
+        if append_our_case_now:
+            append_our_case_now = False
+            directive_length = max(map(lambda item: 0 if item['name'] is None else len(item['name']), suggested_list))
+            for suggested_command in suggested_list:
+                if suggested_command['name'] is None:
+                    print('%s' % suggested_command['args'])
+                else:
+                    print('\t%s  %s' % (suggested_command['name'].ljust(directive_length), suggested_command['args']))
+            print('')
+        print(line, end='')
+    return True
+
 
 print('Scanning %s' % basedir, file=sys.stderr)
 
 props = load_property_file('src/main/resources/ru/mystamps/i18n/ValidationMessages.properties')
+props.update(load_property_file('src/main/resources/ru/mystamps/i18n/Messages.properties'))
+
 constants = load_constants()
 
-for filepath in glob.glob('%s/*.java' % basedir):
+first_test_method_proccessed = False
+processed_java_method = ''
+processed_java_file = ''
+processed_robot_file = ''
+
+javaFiles2robot = {
+    'WhenAdminAtIndexPage.java' : 'site/misc-admin.robot',
+    'WhenUserAtIndexPage.java'  : 'site/misc-user.robot',
+}
+
+for filepath in sorted(glob.glob('%s/*.java' % basedir)):
     filename = os.path.basename(filepath)
-    if filename != 'WhenAnonymousUserActivateAccount.java':
-        continue
-    print('Processing %s' % filename, file=sys.stderr)
+    if first_test_method_proccessed:
+        break
     inside_test_method = 0
     test_method_body = []
     inside_test_method_num = 0
-    for line in fileinput.input(filepath, inplace=True):
+    for line in fileinput.FileInput(filepath, inplace=True):
         if string.find(line, '@Test') > 0:
             inside_test_method = 1
             inside_test_method_num += 1
+            # the 2nd because the first test is usually shouldHaveStandardStructure()
+            if inside_test_method_num == 2:
+                first_test_method_proccessed = True
             test_method_body.append(line.strip())
         else:
             if string.find(line, '{') > 0 and not string.find(line, '}') > 0:
@@ -193,16 +253,15 @@ for filepath in glob.glob('%s/*.java' % basedir):
                     indent = '\t' * line_indent_count
                     test_method = filter(None, ''.join(test_method_body).replace(') {', ') {;').split(';'))
                     #print('%s/// BODY: "%s"' % (indent, test_method), file=sys.stderr)
-                    # TODO: print suggested commands to the robot file
-                    #suggested_list = handleTestMethod(test_method)
-                    #if len(suggested_list) > 0:
-                    #    print('%s/// FIXME' % indent)
-                    #    directive_length = max(map(lambda item: 0 if item['name'] is None else len(item['name']), suggested_list))
-                    #    for suggested_command in suggested_list:
-                    #        if suggested_command['name'] is None:
-                    #            print('%s/// %s' % (indent, suggested_command['args']))
-                    #        else:
-                    #            print('%s///\t%s  %s' % (indent, suggested_command['name'].ljust(directive_length), suggested_command['args']))
+                    # the 2nd because the first test is usually shouldHaveStandardStructure()
+                    if inside_test_method_num == 2:
+                        if write_robot_test(filename, test_method):
+                            processed_java_file = filename
+                            processed_robot_file = javaFiles2robot[filename]
+                            processed_java_method = 'XXX'
+                            res = re.search(r'void ([^\(]+)\(', test_method[0])
+                            if res != None:
+                                processed_java_method = res.group(1)
                     test_method_body[:] = []
             if inside_test_method == 2:
                 curline = line.strip()
@@ -213,3 +272,8 @@ for filepath in glob.glob('%s/*.java' % basedir):
         if string.find(line, '///') < 0 and inside_test_method_num != 2:
             print(line, end='')
 
+    if first_test_method_proccessed == True:
+        print('Processed %s' % filename, file=sys.stderr)
+        print("\nUse the following command to commit results:\n\ngit commit -m '%s.%s(): port to Robot Framework.\n\nAddressed to #530\n\nNo functional changes.' %s %s" % (processed_java_file.rstrip('.java'), processed_java_method, os.path.join(basedir, processed_java_file), os.path.join(robotdir, processed_robot_file)), file=sys.stderr)
+    else:
+        print('Skipped %s' % filename, file=sys.stderr)
