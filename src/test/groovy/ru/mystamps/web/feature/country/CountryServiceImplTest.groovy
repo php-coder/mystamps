41ad59dedb77a86c13f6356c54fb25f98544cf19
Slava Semushin <slava.semushin@gmail.com>
2018-07-19 22:10:55 +0200

    CountryServiceImpl: move the service with related tests to ru.mystamps.web.feature.country package.
    Addressed to #927
    
    No functional changes.

--- src/test/groovy/ru/mystamps/web/feature/country/CountryServiceImplTest.groovy
+++ src/test/groovy/ru/mystamps/web/feature/country/CountryServiceImplTest.groovy
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package ru.mystamps.web.service
+package ru.mystamps.web.feature.country
 
 import static io.qala.datagen.RandomShortApi.nullOr
 import static io.qala.datagen.RandomShortApi.nullOrBlank
@@ -30,8 +30,7 @@ import org.slf4j.helpers.NOPLogger
 import ru.mystamps.web.dao.CountryDao
 import ru.mystamps.web.dao.dto.AddCountryDbDto
 import ru.mystamps.web.dao.dto.LinkEntityDto
-import ru.mystamps.web.feature.country.AddCountryForm
-import ru.mystamps.web.feature.country.CountryService
+import ru.mystamps.web.service.TestObjects
 import ru.mystamps.web.tests.DateUtils
 import ru.mystamps.web.tests.Random
 import ru.mystamps.web.util.SlugUtils
