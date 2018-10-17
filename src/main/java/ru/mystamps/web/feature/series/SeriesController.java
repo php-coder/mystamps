php-coder/mystamps

commit c2a1c00bde131bb7a14bc9f8f1c7907e94bb5b4b

refactor(AddSeriesForm,AddImageForm): rename image to uploadedImage.
Fixing two issues in the same commit because both fields depends on
DownloadImageInterceptor.UPLOADED_IMAGE_FIELD_NAME constant and have to be renamed simultaneously.

Fix #653
Fix #654

No functional changes.

src/main/java/ru/mystamps/web/feature/series/SeriesController.java
@@ -214,7 +214,7 @@ public class SeriesController {
 			addYearToModel(model);
 			
 			// don't try to re-display file upload field
-			form.setImage(null);
+			form.setUploadedImage(null);
 			form.setDownloadedImage(null);
 			return null;
 		}
@@ -329,7 +329,7 @@ public class SeriesController {
 			addStampsToCollectionForm(model, series);
 			
 			// don't try to re-display file upload field
-			form.setImage(null);
+			form.setUploadedImage(null);
 			
 			return "series/info";
 		}

src/main/java/ru/mystamps/web/controller/interceptor/DownloadImageInterceptor.java
@@ -61,7 +61,7 @@ public class DownloadImageInterceptor extends HandlerInterceptorAdapter {
 	 * When it's present, we won't download an image from URL because a user should choose only one
 	 * image source.
 	 */
-	public static final String UPLOADED_IMAGE_FIELD_NAME = "image";
+	public static final String UPLOADED_IMAGE_FIELD_NAME = "uploadedImage";
 	
 	/**
 	 * Name of request attribute, that will be used for storing an error code.

src/main/java/ru/mystamps/web/feature/series/AddImageForm.java
@@ -48,7 +48,7 @@ public class AddImageForm implements AddImageDto, HasImageOrImageUrl, NullableIm
 	@NotEmptyFile(groups = Group.Level1.class)
 	@MaxFileSize(value = MAX_IMAGE_SIZE, unit = Unit.Kbytes, groups = Group.Level2.class)
 	@ImageFile(groups = Group.Level2.class)
-	private MultipartFile image;
+	private MultipartFile uploadedImage;
 	
 	// Name of this field must match with the value of DownloadImageInterceptor.URL_PARAMETER_NAME.
 	@URL(groups = ImageUrl2Checks.class)
@@ -65,7 +65,7 @@ public class AddImageForm implements AddImageDto, HasImageOrImageUrl, NullableIm
 	@Override
 	public MultipartFile getImage() {
 		if (hasImage()) {
-			return image;
+			return uploadedImage;
 		}
 		
 		return downloadedImage;
@@ -75,7 +75,7 @@ public class AddImageForm implements AddImageDto, HasImageOrImageUrl, NullableIm
 	// The latter is being used by RequireImageOrImageUrl validator.
 	@Override
 	public boolean hasImage() {
-		return image != null && StringUtils.isNotEmpty(image.getOriginalFilename());
+		return uploadedImage != null && StringUtils.isNotEmpty(uploadedImage.getOriginalFilename());
 	}
 	
 	// This method has to be implemented to satisfy HasImageOrImageUrl requirements.

src/main/java/ru/mystamps/web/feature/series/AddSeriesForm.java
@@ -151,7 +151,7 @@ public class AddSeriesForm implements AddSeriesDto, HasImageOrImageUrl, Nullable
 	@NotEmptyFile(groups = Image1Checks.class)
 	@MaxFileSize(value = MAX_IMAGE_SIZE, unit = Unit.Kbytes, groups = Image2Checks.class)
 	@ImageFile(groups = Image2Checks.class)
-	private MultipartFile image;
+	private MultipartFile uploadedImage;
 	
 	// Name of this field must match with the value of DownloadImageInterceptor.URL_PARAMETER_NAME.
 	@URL(groups = ImageUrl2Checks.class)
@@ -168,7 +168,7 @@ public class AddSeriesForm implements AddSeriesDto, HasImageOrImageUrl, Nullable
 	@Override
 	public MultipartFile getImage() {
 		if (hasImage()) {
-			return image;
+			return uploadedImage;
 		}
 		
 		return downloadedImage;
@@ -178,7 +178,7 @@ public class AddSeriesForm implements AddSeriesDto, HasImageOrImageUrl, Nullable
 	// The latter is being used by RequireImageOrImageUrl validator.
 	@Override
 	public boolean hasImage() {
-		return image != null && StringUtils.isNotEmpty(image.getOriginalFilename());
+		return uploadedImage != null && StringUtils.isNotEmpty(uploadedImage.getOriginalFilename());
 	}
 	
 	// This method has to be implemented to satisfy HasImageOrImageUrl requirements.

src/main/webapp/WEB-INF/views/series/add.html
@@ -222,7 +222,7 @@
 							</div>
 						</div>
 						
-						<div class="form-group form-group-sm" th:classappend="${#fields.hasErrors('image') ? 'has-error' : ''}">
+						<div class="form-group form-group-sm" th:classappend="${#fields.hasErrors('uploadedImage') ? 'has-error' : ''}">
 							<label for="image" class="control-label col-sm-3">
 								<span th:remove="tag" th:text="#{t_image}">
 									Image
@@ -236,7 +236,7 @@
 									type="file"
 									style="box-shadow: none; border: 0px;"
 									accept="image/png,image/jpeg"
-									th:field="*{image}"
+									th:field="*{uploadedImage}"
 									th:attr="required=${#authorization.expression('hasAuthority(''DOWNLOAD_IMAGE'')') ? null : 'required'}" />
 								<small togglz:active="ADD_ADDITIONAL_IMAGES_TO_SERIES" sec:authorize="hasAuthority('ADD_IMAGES_TO_SERIES')">
 									<span class="hint-block" th:text="#{t_add_more_images_hint}">
@@ -244,7 +244,7 @@
 									</span>
 								</small>
 								<!--/*/
-								<span id="image.errors" class="help-block" th:if="${#fields.hasErrors('image')}" th:each="error : ${#fields.errors('image')}" th:text="${error}"></span>
+								<span id="image.errors" class="help-block" th:if="${#fields.hasErrors('uploadedImage')}" th:each="error : ${#fields.errors('uploadedImage')}" th:text="${error}"></span>
 								/*/-->
 							</div>
 						</div>

src/main/webapp/WEB-INF/views/series/info.html
@@ -98,7 +98,7 @@
 										enctype="multipart/form-data"
 										th:action="@{${ADD_IMAGE_SERIES_PAGE}(id=${series.id})}"
 										th:object="${addImageForm}">
-										<div class="form-group form-group-sm" th:classappend="${#fields.hasErrors('image') ? 'has-error' : ''}">
+										<div class="form-group form-group-sm" th:classappend="${#fields.hasErrors('uploadedImage') ? 'has-error' : ''}">
 											<label for="image" class="control-label col-sm-3" th:text="#{t_image}">
 												Image
 											</label>
@@ -107,10 +107,10 @@
 													type="file"
 													style="box-shadow: none; border: 0px;"
 													accept="image/png,image/jpeg"
-													th:field="*{image}"
+													th:field="*{uploadedImage}"
 													th:attr="required=${#authorization.expression('hasAuthority(''DOWNLOAD_IMAGE'')') ? null : 'required'}" />
 												<!--/*/
-												<span id="image.errors" class="help-block" th:if="${#fields.hasErrors('image')}" th:each="error : ${#fields.errors('image')}" th:text="${error}"></span>
+												<span id="image.errors" class="help-block" th:if="${#fields.hasErrors('uploadedImage')}" th:each="error : ${#fields.errors('uploadedImage')}" th:text="${error}"></span>
 												/*/-->
 											</div>
 										</div>

src/test/groovy/ru/mystamps/web/feature/series/SeriesServiceImplTest.groovy
@@ -416,7 +416,7 @@ class SeriesServiceImplTest extends Specification {
 	
 	def "add() should pass image to image service"() {
 		given:
-			form.setImage(multipartFile)
+			form.setUploadedImage(multipartFile)
 		when:
 			service.add(form, Random.userId(), bool())
 		then:
@@ -501,7 +501,7 @@ class SeriesServiceImplTest extends Specification {
 	
 	def "addImageToSeries() should save image"() {
 		given:
-			imageForm.setImage(multipartFile)
+			imageForm.setUploadedImage(multipartFile)
 		when:
 			service.addImageToSeries(imageForm, Random.id(), Random.userId())
 		then:

commit a613e60373063c55cb4a620726ec48f4ab6d1fa6

fix: error message hasn't been shown when user doesn't have permissions to download a file but tries to do so.
Fix another regression after c2a1c00bde131bb7a14bc9f8f1c7907e94bb5b4b commit.

src/main/java/ru/mystamps/web/feature/series/SeriesController.java
@@ -568,7 +568,7 @@ public class SeriesController {
 					// didn't specify a file but somehow provide a URL to an image. In this case,
 					// let's show an error message that file is required.
 					result.rejectValue(
-						"image",
+						DownloadImageInterceptor.UPLOADED_IMAGE_FIELD_NAME,
 						"ru.mystamps.web.support.beanvalidation.NotEmptyFilename.message"
 					);
 					form.nullifyImageUrl();