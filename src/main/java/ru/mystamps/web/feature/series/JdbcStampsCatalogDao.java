0892f935f7557f78477788ed9735b960bf5c406e
Slava Semushin <slava.semushin@gmail.com>
2019-02-14 23:42:19 +0000

    refactor(JdbcStampsCatalogDao): move to ru.mystamps.web.feature.series package.
    Addressed to #927
    
    No functional changes.

--- src/main/java/ru/mystamps/web/feature/series/JdbcStampsCatalogDao.java
+++ src/main/java/ru/mystamps/web/feature/series/JdbcStampsCatalogDao.java
@@ -15,12 +15,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package ru.mystamps.web.dao.impl;
+package ru.mystamps.web.feature.series;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.Validate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import ru.mystamps.web.feature.series.StampsCatalogDao;
 
 import java.util.ArrayList;
 import java.util.Collections;
