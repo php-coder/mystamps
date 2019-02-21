493774a562015fba1fde90e98174245957300e8e
Slava Semushin <slava.semushin@gmail.com>
2019-02-21 22:36:15 +0000

    refactor: removed unused package-info.java from ru.mystamps.web.service.dto package.
    No code changes.
    
    [ckip ci]

--- src/main/java/ru/mystamps/web/service/dto/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Service layer: <a href="https://en.wikipedia.org/wiki/Data_transfer_object"
- * target="_blank">DTO</a> classes that are being used by service classes as
- * input/output values.
- */
-package ru.mystamps.web.service.dto;
