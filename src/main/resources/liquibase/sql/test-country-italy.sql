604bd1aa8649138aab7648d3d13e73045543c65b
Slava Semushin <slava.semushin@gmail.com>
2019-05-07 16:09:56 +0100

    refactor(test/spring/test-data.properties): remove unused valid_country_name_* properties.
    Should be in 048b6ab100c66cfdef2699a42f021753046b08b2 commit.
    
    No functional changes.

--- src/main/resources/liquibase/sql/test-country-italy.sql
+++ src/main/resources/liquibase/sql/test-country-italy.sql
@@ -4,8 +4,8 @@
 
 -- Used below as country's owner
 INSERT INTO users(id, login, role, name, registered_at, activated_at, hash, salt, email) VALUES
-	(3, 'test0', 'USER', '@valid_country_name_en@ Country Owner', NOW(), NOW(), '@old_valid_user_password_hash@', '@old_valid_user_password_salt@', 'test0@example.org');
+	(3, 'test0', 'USER', 'Italy Country Owner', NOW(), NOW(), '@old_valid_user_password_hash@', '@old_valid_user_password_salt@', 'test0@example.org');
 
 -- Used at least by src/test/robotframework/country/creation/validation.robot
 INSERT INTO countries(name, created_at, created_by, updated_at, updated_by) VALUES
-	('@valid_country_name_en@', NOW(), 3, NOW(), 3);
+	('Italy', NOW(), 3, NOW(), 3);
