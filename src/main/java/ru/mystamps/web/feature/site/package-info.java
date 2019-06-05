362719bd645ef5fabf1a4b2f687222744564c253
Slava Semushin <slava.semushin@gmail.com>
2019-06-05 18:51:31 +0100

    refactor(ErrorController): move to ru.mystamps.web.feature.site package.
    Addressed to #927
    
    No functional changes.

---
+++ src/main/java/ru/mystamps/web/feature/site/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Assorted site-specific things.
+ *
+ * The many aspects of the site that are too small to be extracted into a separate feature package.
+ * Also all of them are specific to the site and can't be re-used somewhere else. Here is the list
+ * of the things that belongs to this package:
+ * <ul>
+ *     <li>main and error pages</li>
+ *     <li>robots.txt and sitemap.xml files</li>
+ *     <li>recurring maintenance tasks</li>
+ *     <li>all mailing-related activities (for instance, sending a daily report to admin)</li>
+ *     <li>tracking of the suspicious activity</li>
+ * </ul>
+ */
+package ru.mystamps.web.feature.site;
