1bc64499e63e4b1451a8a8f20ddad750f45343ab
Slava Semushin <slava.semushin@gmail.com>
2019-06-12 22:23:51 +0100

    fix(collections): use autogenerated ids to prevent duplicate key violations on insertion later.
    See cdb000eb0a0ef9458d2f006e8c4dbe2342ad4a7a commit for details.
    
    Addressed to #1034

--- src/main/resources/liquibase/sql/test-user-paid.sql
+++ src/main/resources/liquibase/sql/test-user-paid.sql
@@ -5,5 +5,10 @@
 INSERT INTO users(id, login, role, name, registered_at, activated_at, hash, email) VALUES
 	(5, 'paid', 'PAID', 'Paid User', NOW(), NOW(), '$2a$10$8Rxlvw8r7r7a.w5rxOJYY.XbBE71ivvGjlnE6w/G73A58l1I76VRK' /* test */, 'paid-user@example.com');
 
-INSERT INTO collections(id, user_id, slug, updated_at, updated_by) VALUES
-	(5, 5, 'paid', NOW(), 5);
+INSERT INTO collections(user_id, slug, updated_at, updated_by) VALUES
+	(
+		(SELECT id FROM users WHERE login = 'paid'),
+		'paid',
+		NOW(),
+		(SELECT id FROM users WHERE login = 'paid')
+	);
