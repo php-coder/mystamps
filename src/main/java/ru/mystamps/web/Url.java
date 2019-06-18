4a1c67241da4ccf4c773e62ac9e0079b41bbce15
Slava Semushin <slava.semushin@gmail.com>
2019-06-18 22:14:52 +0100

    refactor(Url): reduce visibility of most of the constants to private.
    Addressed to #927
    
    No functional changes.

--- src/main/java/ru/mystamps/web/Url.java
+++ src/main/java/ru/mystamps/web/Url.java
@@ -40,42 +40,43 @@ import java.util.Map;
  * @author Slava Semushin
  */
 public final class Url {
-	public static final String STATIC_RESOURCES_URL  = "https://stamps.filezz.ru";
+	
+	// see also src/main/resources/application-test.properties
+	public static final String H2_CONSOLE_PAGE = "/console";
 	
 	// MUST be updated when any of our resources were modified
-	public static final String RESOURCES_VERSION      = "v0.3.13";
+	public static final String RESOURCES_VERSION = "v0.3.13";
+	
+	private static final String STATIC_RESOURCES_URL = "https://stamps.filezz.ru";
 	
 	// CheckStyle: ignore LineLength for next 7 lines
-	public static final String MAIN_CSS               = "/static/" + RESOURCES_VERSION + "/styles/main.min.css";
-	public static final String CATALOG_UTILS_JS       = "/public/js/" + RESOURCES_VERSION + "/CatalogUtils.min.js";    // NOPMD: AvoidDuplicateLiterals
-	public static final String DATE_UTILS_JS          = "/public/js/" + RESOURCES_VERSION + "/DateUtils.min.js";       // NOPMD: AvoidDuplicateLiterals
-	public static final String PARTICIPANT_ADD_JS     = "/public/js/" + RESOURCES_VERSION + "/participant/add.min.js"; // NOPMD: AvoidDuplicateLiterals
-	public static final String SERIES_ADD_JS          = "/public/js/" + RESOURCES_VERSION + "/series/add.min.js";      // NOPMD: AvoidDuplicateLiterals
-	public static final String SERIES_INFO_JS         = "/public/js/" + RESOURCES_VERSION + "/series/info.min.js";     // NOPMD: AvoidDuplicateLiterals
-	public static final String COLLECTION_INFO_JS     = "/public/js/" + RESOURCES_VERSION + "/collection/info.min.js"; // NOPMD: AvoidDuplicateLiterals
-	public static final String FAVICON_ICO            = "/favicon.ico";
+	private static final String MAIN_CSS           = "/static/" + RESOURCES_VERSION + "/styles/main.min.css";
+	private static final String CATALOG_UTILS_JS   = "/public/js/" + RESOURCES_VERSION + "/CatalogUtils.min.js";    // NOPMD: AvoidDuplicateLiterals
+	private static final String DATE_UTILS_JS      = "/public/js/" + RESOURCES_VERSION + "/DateUtils.min.js";       // NOPMD: AvoidDuplicateLiterals
+	private static final String PARTICIPANT_ADD_JS = "/public/js/" + RESOURCES_VERSION + "/participant/add.min.js"; // NOPMD: AvoidDuplicateLiterals
+	private static final String SERIES_ADD_JS      = "/public/js/" + RESOURCES_VERSION + "/series/add.min.js";      // NOPMD: AvoidDuplicateLiterals
+	private static final String SERIES_INFO_JS     = "/public/js/" + RESOURCES_VERSION + "/series/info.min.js";     // NOPMD: AvoidDuplicateLiterals
+	private static final String COLLECTION_INFO_JS = "/public/js/" + RESOURCES_VERSION + "/collection/info.min.js"; // NOPMD: AvoidDuplicateLiterals
+	private static final String FAVICON_ICO        = "/favicon.ico";
 	
 	// CheckStyle: ignore LineLength for next 4 lines
-	public static final String BOOTSTRAP_CSS          = "/public/bootstrap/3.4.1/css/bootstrap.min.css";
-	public static final String BOOTSTRAP_JS           = "/public/bootstrap/3.4.1/js/bootstrap.min.js";
-	public static final String JQUERY_JS              = "/public/jquery/1.9.1/jquery.min.js";
-	public static final String BOOTSTRAP_LANGUAGE     = "https://cdn.jsdelivr.net/gh/usrz/bootstrap-languages@3ac2a3d2b27ac43a471cd99e79d378a03b2c6b5f/languages.min.css";
+	private static final String BOOTSTRAP_CSS      = "/public/bootstrap/3.4.1/css/bootstrap.min.css";
+	private static final String BOOTSTRAP_JS       = "/public/bootstrap/3.4.1/js/bootstrap.min.js";
+	private static final String JQUERY_JS          = "/public/jquery/1.9.1/jquery.min.js";
+	private static final String BOOTSTRAP_LANGUAGE = "https://cdn.jsdelivr.net/gh/usrz/bootstrap-languages@3ac2a3d2b27ac43a471cd99e79d378a03b2c6b5f/languages.min.css";
 	
 	// CheckStyle: ignore LineLength for next 3 lines
 	// FIXME: use minimal version of CSS file when it will be available (https://github.com/webjars/selectize.js/issues/3)
-	public static final String SELECTIZE_CSS          = "/public/selectize/0.12.5/css/selectize.bootstrap3.css";
-	public static final String SELECTIZE_JS           = "/public/selectize/0.12.5/js/standalone/selectize.min.js";
+	private static final String SELECTIZE_CSS      = "/public/selectize/0.12.5/css/selectize.bootstrap3.css";
+	private static final String SELECTIZE_JS       = "/public/selectize/0.12.5/js/standalone/selectize.min.js";
 	
 	// see also pom.xml and ru.mystamps.web.config.MvcConfig#addResourceHandlers()
 	// CheckStyle: ignore LineLength for next 5 lines
-	public static final String BOOTSTRAP_CSS_CDN     = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css";
-	public static final String BOOTSTRAP_JS_CDN      = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js";
-	public static final String JQUERY_JS_CDN         = "https://yandex.st/jquery/1.9.1/jquery.min.js";
-	public static final String SELECTIZE_CSS_CDN     = "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.bootstrap3.min.css";
-	public static final String SELECTIZE_JS_CDN      = "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js";
-	
-	// see also src/main/resources/application-test.properties
-	public static final String H2_CONSOLE_PAGE       = "/console";
+	private static final String BOOTSTRAP_CSS_CDN  = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css";
+	private static final String BOOTSTRAP_JS_CDN   = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js";
+	private static final String JQUERY_JS_CDN      = "https://yandex.st/jquery/1.9.1/jquery.min.js";
+	private static final String SELECTIZE_CSS_CDN  = "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.bootstrap3.min.css";
+	private static final String SELECTIZE_JS_CDN   = "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js";
 	
 	private Url() {
 	}
