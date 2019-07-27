94e87d5fde554c5224da8d410d61473d4aea9871
Mukesh Katariya <jmukesh@gmail.com>
2019-07-27 19:53:25 +0530

    task: include userId in the log messages by using MDC.
    Related links:
    - http://logback.qos.ch/manual/mdc.html
    - http://docs.spring.io/spring-boot/docs/1.5.x/reference/html/boot-features-logging.html
    
    Part of #388
    Fix #1102

---
+++ src/main/java/ru/mystamps/web/support/spring/security/UserMdcLoggingFilter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2009-2019 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package ru.mystamps.web.support.spring.security;
+
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserMdcLoggingFilter extends OncePerRequestFilter {
+	private static final String USER_ID = "userId";
+	
+	@Override
+	protected void doFilterInternal(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		FilterChain filterChain)
+		throws ServletException, IOException {
+
+		Integer userId = SecurityContextUtils.getUserId();
+		if (userId != null) {
+			MDC.put(USER_ID, userId.toString());
+		}
+		
+		try {
+			filterChain.doFilter(request, response);
+		} finally {
+			MDC.remove(USER_ID);
+		}
+	}
+	
+}
