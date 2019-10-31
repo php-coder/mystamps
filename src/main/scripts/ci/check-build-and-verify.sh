87ec1f13c25bef37c6de7418a8683654ed0cb1c3
Slava Semushin <slava.semushin@gmail.com>
2019-10-31 22:26:16 +0000

    refactor: inline common.sh into check-build-and-verify.sh script.
    common.sh was used by more than one script until 471a8715131761462c6669ca92c4f979a5766647 commit
    when we removed another script. Now there is no value to have more files than needed.

--- src/main/scripts/ci/check-build-and-verify.sh
+++ src/main/scripts/ci/check-build-and-verify.sh
@@ -9,14 +9,40 @@ set -o errexit
 # Return value of a pipeline is the value of the last command to exit with a non-zero status
 set -o pipefail
 
+print_status() {
+	# $1 is empty if check has succeeded
+	# $1 equals to 'fail' if check has failed
+	# $1 equals to 'skip' if check has skipped
+	local result="$1"
+	local msg="$2"
+	
+	local status='SUCCESS'
+	local color=32
+	
+	if [ "$result" = 'fail' ]; then
+		status='FAIL'
+		color=31
+	elif [ "$result" = 'skip' ]; then
+		status='SKIP'
+		color=33
+	fi
+	printf "* %s... \033[1;%dm%s\033[0m\n" "$msg" "$color" "$status"
+}
+
+print_log() {
+	local log_file="$1"
+	local msg="$2"
+	
+	echo
+	printf "=====> \033[1;33m%s\033[0m\n" "$msg"
+	echo
+	grep -Ev '^\[INFO\] Download(ing|ed)' "$log_file" || :
+}
 
 RUN_ONLY_INTEGRATION_TESTS=no
 if [ "${1:-}" = '--only-integration-tests' ]; then
 	RUN_ONLY_INTEGRATION_TESTS=yes
 fi
-
-# shellcheck source=src/main/scripts/ci/common.sh
-. "$(dirname "$0")/common.sh"
 
 CS_STATUS=
 PMD_STATUS=
