2b547d9212fbd7a02eeb7c83885a9e8821d70a18
Slava Semushin <slava.semushin@gmail.com>
2021-01-10 23:42:44 +0700

    chore: configure jest to send output to stdout instead of stderr
    See for details:
    https://github.com/facebook/jest/issues/5064#issuecomment-401451361
    
    Relate to #1484

--- src/main/frontend/jest.config.js
+++ src/main/frontend/jest.config.js
@@ -1,5 +1,8 @@
 // @todo #1484 Document Jest usage
 module.exports = {
 	// https://jestjs.io/docs/en/configuration#watchman-boolean
-	"watchman": false
+	"watchman": false,
+	// See: https://github.com/facebook/jest/issues/5064#issuecomment-401451361
+	// @todo #1484 Remove usage of jest-standard-reporter once facebook/jest#5064 is resolved
+	"reporters": [ "jest-standard-reporter" ]
 }
