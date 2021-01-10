bacab7b54b5fddb7b5ba6ece039e225d116a0f89
Slava Semushin <slava.semushin@gmail.com>
2021-01-10 23:53:10 +0700

    chore: transform our tests from ESM to CJS to make them understandable by Jest
    Previously, tests that use "import" were failing:
    [ERROR]   ● Test suite failed to run
    [ERROR]
    [ERROR]     Jest encountered an unexpected token
    [ERROR]
    [ERROR]     This usually means that you are trying to import a file which Jest cannot parse, e.g. it's not plain JavaScript.
    [ERROR]
    [ERROR]     By default, if Jest sees a Babel config, it will use that to transform your files, ignoring "node_modules".
    [ERROR]
    [ERROR]     Here's what you can do:
    [ERROR]      • If you are trying to use ECMAScript Modules, see https://jestjs.io/docs/en/ecmascript-modules for how to enable it.
    [ERROR]      • To have some of your "node_modules" files transformed, you can specify a custom "transformIgnorePatterns" in your config.
    [ERROR]      • If you need a custom transformation specify a "transform" option in your config.
    [ERROR]      • If you simply want to mock your non-JS modules (e.g. binary assets) you can stub them out with the "moduleNameMapper" config option.
    [ERROR]
    [ERROR]     You'll find more details and examples of these config options in the docs:
    [ERROR]     https://jestjs.io/docs/en/configuration.html
    [ERROR]
    [ERROR]     Details:
    [ERROR]
    [ERROR]     /Users/coder/git/mystamps/src/main/frontend/src/components/AddCommentForm.test.js:1
    [ERROR]     ({"Object.<anonymous>":function(module,exports,require,__dirname,__filename,global,jest){import { render, screen } from '@testing-library/react';
    [ERROR]                                                                                                     ^
    [ERROR]
    [ERROR]     SyntaxError: Unexpected token {
    [ERROR]
    [ERROR]       at Runtime.createScriptFromCode (node_modules/jest-runtime/build/index.js:1350:14)
    
    See also: https://github.com/ActuallyACat/jest-esm-transformer
    
    Part of #1484

--- src/main/frontend/jest.config.js
+++ src/main/frontend/jest.config.js
@@ -2,6 +2,11 @@
 module.exports = {
 	// https://jestjs.io/docs/en/configuration#watchman-boolean
 	"watchman": false,
+	// required to use ECMAScript Modules (ESM)
+	// @todo #1484 Find a better way to use ESM with Jest and replace jest-esm-transformer
+	"transform": {
+		"\\.js$": "jest-esm-transformer"
+	},
 	// See: https://github.com/facebook/jest/issues/5064#issuecomment-401451361
 	// @todo #1484 Remove usage of jest-standard-reporter once facebook/jest#5064 is resolved
 	"reporters": [ "jest-standard-reporter" ]
