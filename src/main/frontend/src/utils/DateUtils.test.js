fa4d96d02f935876fde16c4bf929dfc5534c786b
Slava Semushin <slava.semushin@gmail.com>
2021-11-11 00:03:01 +0700

    refactor(DateUtils.test.js): replace "var" by "const"

--- src/main/frontend/src/utils/DateUtils.test.js
+++ src/main/frontend/src/utils/DateUtils.test.js
@@ -3,7 +3,7 @@ import './DateUtils.js'
 describe('DateUtils.formatDateToDdMmYyyy()', () => {
 	
 	it('should return a string in a format dd.mm.yyyy', () => {
-		var date = new Date('2018-06-12T16:59:54.451Z');
+		const date = new Date('2018-06-12T16:59:54.451Z');
 		expect(DateUtils.formatDateToDdMmYyyy(date)).toEqual('12.06.2018');
 	});
 	
