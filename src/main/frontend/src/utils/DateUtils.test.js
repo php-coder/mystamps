ba1e775b39e51cf7291df42b80515c80c0261884
Slava Semushin <slava.semushin@gmail.com>
2021-11-11 00:06:49 +0700

    refactor(DateUtils.test.js): add given/when/then

--- src/main/frontend/src/utils/DateUtils.test.js
+++ src/main/frontend/src/utils/DateUtils.test.js
@@ -3,8 +3,12 @@ import './DateUtils.js'
 describe('DateUtils.formatDateToDdMmYyyy()', () => {
 	
 	it('should return a string in a format dd.mm.yyyy', () => {
+		// given
 		const date = new Date('2018-06-12T16:59:54.451Z');
-		expect(DateUtils.formatDateToDdMmYyyy(date)).toBe('12.06.2018');
+		// when
+		const result = DateUtils.formatDateToDdMmYyyy(date);
+		// then
+		expect(result).toBe('12.06.2018');
 	});
 	
 });
