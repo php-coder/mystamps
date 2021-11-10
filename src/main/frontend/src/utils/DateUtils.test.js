087e9baccd9411b80386811b5d9c1097e650e661
Slava Semushin <slava.semushin@gmail.com>
2021-11-11 00:03:42 +0700

    refactor(DateUtils.test.js): use matcher toBe() instead of toEqual()
    Quote from https://jestjs.io/docs/using-matchers:
    
    > toBe uses Object.is to test exact equality. If you want to check the value of an object, use toEqual instead

--- src/main/frontend/src/utils/DateUtils.test.js
+++ src/main/frontend/src/utils/DateUtils.test.js
@@ -4,7 +4,7 @@ describe('DateUtils.formatDateToDdMmYyyy()', () => {
 	
 	it('should return a string in a format dd.mm.yyyy', () => {
 		const date = new Date('2018-06-12T16:59:54.451Z');
-		expect(DateUtils.formatDateToDdMmYyyy(date)).toEqual('12.06.2018');
+		expect(DateUtils.formatDateToDdMmYyyy(date)).toBe('12.06.2018');
 	});
 	
 });
