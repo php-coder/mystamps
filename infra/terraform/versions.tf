bcb94022a265b3215502cb40e8a7591d41fbdab3
Slava Semushin <slava.semushin@gmail.com>
2023-05-19 11:15:20 +0700

    chore: update DigitalOcean Terraform Provider to 2.28.1
    $ vim versions.tf
    $ terraform init
    $ terraform plan
    
    Fix #1268
    
    [skip ci]

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -1,10 +1,11 @@
 terraform {
+  # @todo #1268 Update Terraform to 0.15.x
   required_version = ">= 0.13"
 
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.22.2"
+      version = "2.28.1"
     }
   }
 }
