597aabf5c2ca7f5406d81fed067e5e49dc91af1f
Slava Semushin <slava.semushin@gmail.com>
2023-06-12 10:11:16 +0700

    ci: extract todos from code with pdd and save them into pdd.xml and todos-in-code.tsv
    Part of #1610

---
+++ src/main/scripts/ci/pdd-json-to-tsv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Algorithm:
+# 1) sort by ticket and id
+# 2) prepend a header (that will be used by GitHub for preview)
+# 3) transform a list of objects to many object: [{}, {}] => {}, {}
+# 4) transform each object to a list: {}, {} => [], []
+# 4a) surround .body with quotes and escape double quotes inside by doubling them (this is what @csv filter does).
+#     Required to be able to view at GitHub. See also:
+#     https://docs.github.com/en/repositories/working-with-files/using-files/working-with-non-code-files#rendering-csv-and-tsv-data
+# 5) transform lists to TSV rows
+
+exec jq --raw-output '. | sort_by([ .ticket | tonumber ], .id) | [ { "id":"Id", "ticket":"Ticket", "body":"Title", "file":"File", "lines":"Lines" } ] + . | .[] | [ .id, .ticket, ([ .body ] | @csv), .file, .lines ] | @tsv'
