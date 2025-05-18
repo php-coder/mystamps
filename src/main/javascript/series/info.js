664a63fe6ca7513da4db3c251a528f06a4f44c2c
Slava Semushin <slava.semushin@gmail.com>
2025-05-18 17:51:34 +0700

    refactor: extract code for initialization catalog related fields to a function

--- src/main/javascript/series/info.js
+++ src/main/javascript/series/info.js
@@ -22,3 +22,25 @@ function getCurrencyByCatalogName(catalog) {
 			return [ '\u20BD', 'RUB' ];
 	}
 }
+
+function initPriceCatalog() {
+	var catalogNameElem = document.getElementById('price-catalog-name');
+	if (catalogNameElem == null) {
+		console.error("Couldn't initialize catalog name selector: element not found");
+		return;
+	}
+	catalogNameElem.addEventListener('change', function changeCatalogCurrency(elem) {
+		var name = this.value;
+		var info = getCurrencyByCatalogName(this.value);
+		var symbolElem = document.getElementById('js-catalog-price-symbol');
+		if (symbolElem == null) {
+			console.error("Couldn't change currency symbol: element not found");
+		}
+		var titleElem = document.getElementById('catalog-price');
+		if (titleElem == null) {
+			console.error("Couldn't change currency title: element not found");
+		}
+		symbolElem.innerText = info[0];
+		titleElem.title = info[1];
+	});
+}
