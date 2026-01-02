dab30a56ede3ea94436799ae9097ed296c1435ff
Slava Semushin <slava.semushin@gmail.com>
2026-01-02 16:35:23 +0700

    chore: update copyright year
    [skip ci]

--- src/main/java/ru/mystamps/web/support/togglz/Features.java
+++ src/main/java/ru/mystamps/web/support/togglz/Features.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2025 Slava Semushin <slava.semushin@gmail.com>
+ * Copyright (C) 2009-2026 Slava Semushin <slava.semushin@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
