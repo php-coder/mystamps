79c4f88429d1e93cd7681c24f98e4d929bde1e72
Slava Semushin <slava.semushin@gmail.com>
2026-09-23 07:32:18 +0700

    chore: update spring-boot-dependency-checker to 1.4.0
    Changelogs:
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.0.0-RC1
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.0.0-RC2
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.0.0-RC3
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.0.0-RC4
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.0.0
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.1.0
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.1.1
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.2.0
    - https://github.com/mrbusche/spring-boot-dependency-checker/releases/tag/v1.4.0

--- src/main/scripts/show-spring-boot-version-diff.sh
+++ src/main/scripts/show-spring-boot-version-diff.sh
@@ -14,19 +14,10 @@ PROJECT_POM="$CURRENT_DIR/../../../pom.xml"
 
 (
 	printf 'Dependency\tProject\tSpring Boot\n';
-	# Workarounds for https://github.com/mrbusche/spring-boot-dependency-checker/issues/189
-	# and https://github.com/mrbusche/spring-boot-dependency-checker/issues/190
 	spring-boot-dependency-checker "$PROJECT_POM" |
-		sed -e '/^Processing xml file/d' \
-			-e '/^Detected Spring Boot Version/d' \
-			-e '/^Declared Pom Package Count/d' \
-			-e '/^Spring Boot default versions URL no longer exists/d' \
-			-e 's/^Declared Pom Packages - //' \
-			-e 's/Package {/{/' \
-			-e "s/'/\"/g" \
-			-e 's/\(group\|name\|inputFileVersion\|bootVersion\)/"\1"/' |
 		jq --raw-output '
-			sort_by(.group, .name)
+			.packages
+			| sort_by(.group, .name)
 			| .[]
 			| select(.bootVersion != .inputFileVersion)
 			| [ "\(.group):\(.name)", .inputFileVersion, .bootVersion ]
